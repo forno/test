ef9df4890067a8966132d3698e99d3ec7929f6d0
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-08 03:05:14 +0900

    Add inline and reorder arguments of template

--- include/xmaho/string.hpp
+++ include/xmaho/string.hpp
@@ -17,8 +17,8 @@ namespace string
  * The string is splited by delimiter to std::vector.
  * On Javascript, It is know as String.split().
  */
-template<typename StringT, typename DelimiterT>
-std::vector<StringT> split(const StringT& base_text, DelimiterT&& delimiter)
+template<typename DelimiterT, typename StringT>
+inline std::vector<StringT> split(const StringT& base_text, DelimiterT&& delimiter)
 {
   std::basic_regex<typename StringT::value_type> delimiterRegex(std::forward<DelimiterT>(delimiter));
   std::regex_token_iterator<typename StringT::const_iterator>
@@ -27,8 +27,8 @@ std::vector<StringT> split(const StringT& base_text, DelimiterT&& delimiter)
   return {first, last};
 }
 
-template<typename CharT, size_t N, typename DelimiterT>
-std::vector<std::basic_string<CharT>> split(const CharT (&base_text)[N], DelimiterT&& delimiter)
+template<typename DelimiterT, typename CharT, size_t N>
+inline std::vector<std::basic_string<CharT>> split(const CharT (&base_text)[N], DelimiterT&& delimiter)
 {
   return split(std::basic_string<CharT>(base_text), std::forward<DelimiterT>(delimiter));
 }
