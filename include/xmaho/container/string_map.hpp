9b53e34ec4c2e06eb1ca556181ca87baa2fc36e6
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-08 03:27:53 +0900

    Refactor container library

---
+++ include/xmaho/container/string_map.hpp
@@ -0,0 +1,22 @@
+#ifndef XMAHO_XMAHO_CONTAINER_STRING_MAP_H
+#define XMAHO_XMAHO_CONTAINER_STRING_MAP_H
+
+#include <unordered_map>
+
+namespace xmaho
+{
+namespace container
+{
+
+/**
+ * \brief Speedy string map.
+ *
+ * This is unordered map.
+ */
+template<typename T>
+using StringMap = std::unordered_map<std::string, T>;
+
+}
+}
+
+#endif
