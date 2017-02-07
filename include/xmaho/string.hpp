554a9bb57e19e511e92cd132cce83d16d0ba7984
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-08 02:53:42 +0900

    Add string::split for literal string

--- include/xmaho/string.hpp
+++ include/xmaho/string.hpp
@@ -18,14 +18,19 @@ namespace string
  * On Javascript, It is know as String.split().
  */
 template<typename StringT, typename DelimiterT>
-std::vector<StringT> split(const StringT& base_text,
-                           DelimiterT&& delimiter)
+std::vector<StringT> split(const StringT& base_text, DelimiterT&& delimiter)
 {
   std::basic_regex<typename StringT::value_type> delimiterRegex(std::forward<DelimiterT>(delimiter));
   std::regex_token_iterator<typename StringT::const_iterator>
       first {base_text.cbegin(), base_text.cend(), delimiterRegex, -1},
       last;
   return {first, last};
+}
+
+template<typename CharT, size_t N, typename DelimiterT>
+std::vector<std::basic_string<CharT>> split(const CharT (&base_text)[N], DelimiterT&& delimiter)
+{
+  return split(std::basic_string<CharT>(base_text), std::forward<DelimiterT>(delimiter));
 }
 
 }
--- test/string_split.cpp
+++ test/string_split.cpp
@@ -5,4 +5,7 @@
 int main(int argc, char* argv[]) {
   auto result = xmaho::string::split(std::string("hoge"), "o");
   for (auto& x : result) std::cout << x << std::endl;
+  std::cout << "othor test" << std::endl;
+  auto othertest = xmaho::string::split(("nyan"), "a");
+  for (auto& x : othertest) std::cout << x << std::endl;
 }
