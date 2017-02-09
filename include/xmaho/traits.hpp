6fa424280602cdf54398063734b4541dcaf57324
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-09 20:10:30 +0900

    Add documents of traits.hpp and traits namespace

--- include/xmaho/traits.hpp
+++ include/xmaho/traits.hpp
@@ -1,6 +1,16 @@
 #ifndef XMAHO_XMAHO_TRAITS_H
 #define XMAHO_XMAHO_TRAITS_H
 
+/**
+ * @file traits.hpp
+ * @brief This call traits/*.hpp.
+ */
+
+/**
+ * @namespace xmaho::traits
+ * @brief The namespace for traits library.
+ */
+
 #include "xmaho/traits/enable_if.hpp"
 
 #endif
