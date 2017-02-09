2810a76fe5ed27b19a9ea8ea7655a9be88271426
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-09 11:33:23 +0900

    Update test code of string::split with googletest

--- test/string/split.cpp
+++ test/string/split.cpp
@@ -1,32 +1,57 @@
-#include <iostream>
 #include <regex>
 #include <string>
 
+#include <gtest/gtest.h>
+
 #include "xmaho/string.hpp"
 
-int main(int argc, char* argv[]) {
-  {
-    std::cout << "[[test]]" << std::endl;
-    std::string text("hogehogehoge");
-    std::regex delim("o");
-    auto result = xmaho::string::split(text, delim);
-    for (auto& x : result) std::cout << x << std::endl;
-  }
-  {
-    std::cout << "[[test]]" << std::endl;
-    std::string text("hogehogehoge");
-    std::string delim("o");
-    auto result = xmaho::string::split(text, delim);
-    for (auto& x : result) std::cout << x << std::endl;
-  }
-  {
-    std::cout << "[[test]]" << std::endl;
-    auto result = xmaho::string::split(std::string("hoge"), "o");
-    for (auto& x : result) std::cout << x << std::endl;
-  }
-  {
-    std::cout << "[[test]]" << std::endl;
-    auto result = xmaho::string::split(("nyan"), "a");
-    for (auto& x : result) std::cout << x << std::endl;
-  }
+TEST(BaseTest, HandlesNullInput)
+{
+  std::string base {};
+  std::regex delimiter {};
+  auto result = xmaho::string::split(base, delimiter);
+  ASSERT_EQ(result, std::vector<std::string>{});
+}
+
+TEST(BaseTest, HandlesNormalInput)
+{
+  std::string base("a,b,c");
+  std::regex delimiter(",");
+  auto result = xmaho::string::split(base, delimiter);
+  std::vector<std::string> ans {"a", "b", "c"};
+  ASSERT_EQ(result, ans);
+}
+
+TEST(DelimiterForwardTest, HandlesNullInput)
+{
+  std::string base {};
+  std::string delimiter {};
+  auto result = xmaho::string::split(base, delimiter);
+  ASSERT_EQ(result, std::vector<std::string>{});
+}
+
+TEST(DelimiterForwardTest, HandlesNormalInput)
+{
+  std::string base("a,b,c");
+  std::string delimiter(",");
+  auto result = xmaho::string::split(base, delimiter);
+  std::vector<std::string> ans {"a", "b", "c"};
+  ASSERT_EQ(result, ans);
+}
+
+TEST(StringLiteralTest, HandlesNullInput)
+{
+  auto base = "";
+  auto delimiter = "";
+  auto result = xmaho::string::split(base, delimiter);
+  ASSERT_EQ(result, std::vector<std::string>{});
+}
+
+TEST(StringLiteralTest, HandleNormalInput)
+{
+  auto base = "a,b,c";
+  auto delimiter = ",";
+  auto result = xmaho::string::split(base, delimiter);
+  std::vector<std::string> ans {"a", "b", "c"};
+  ASSERT_EQ(result, ans);
 }
