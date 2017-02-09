338360fa6580f02128bce96cca8225a6fd2d5eea
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-09 20:04:15 +0900

    Add document of string.hpp and string namespace

--- include/xmaho/string.hpp
+++ include/xmaho/string.hpp
@@ -1,6 +1,16 @@
 #ifndef XMAHO_XMAHO_STRING_H
 #define XMAHO_XMAHO_STRING_H
 
+/**
+ * @file string.hpp
+ * @brief This call string/*.hpp.
+ */
+
+/**
+ * @namespace string
+ * @brief The namespace for string library.
+ */
+
 #include "xmaho/string/split.hpp"
 
 #endif
