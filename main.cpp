95b45959034fdbdef04bfe14534ddb650c3005cc
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-17 21:12:49 +0900

    Test regex_token_iterator: OK/regex is explicit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include <regex>
+#include <string>
+#include <type_traits>
+#include <vector>
 
 int main(int argc, char** argv) {
-  std::cout << "Hello World" << std::endl;
+  std::vector<std::string> input (argv, argv + argc);
+
+  for (const auto& e : input) {
+    std::regex delimiter {","};
+    std::regex_token_iterator<std::remove_reference<decltype(e)>::type::const_iterator> start {e.cbegin(), e.cend(), delimiter, -1},
+                                                                                        end {};
+    for (;start != end; ++start)
+      std::cout << *start << ' ';
+    std::cout << std::endl;
+  }
 }
