36e8c84a3f9cf48f49ba1b91c2046d41a6b0859b
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-18 00:03:21 +0900

    Add type_alias and remove_const_t

---
+++ include/xmaho/traits/type_alias.hpp
@@ -0,0 +1,25 @@
+#ifndef XMAHO_XMAHO_TRAITS_TYPE_ALIAS_H
+#define XMAHO_XMAHO_TRAITS_TYPE_ALIAS_H
+
+/**
+ * @file traits/type_alias.hpp
+ * @brief This have some convertion to **_t from **::type.
+ */
+
+#include <type_traits>
+
+namespace xmaho
+{
+namespace traits
+{
+
+/**
+ * @brief remove_const_t for c++11.
+ */
+template<typename T>
+using remove_const_t<T> = std::remove_const<T>::type;
+
+}
+}
+
+#endif
