7b67dce8f10f0e2366a061e79efe44d797c290da
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-19 03:49:50 +0900

    Qiita: template function over load

--- main.cpp
+++ main.cpp
@@ -4,15 +4,30 @@
 #include <type_traits>
 #include <vector>
 
-int main(int argc, char** argv) {
-  std::vector<std::string> input (argv, argv + argc);
+template<typename T, typename S>
+bool func(const T& lhs, const S& rhs) {
+  std::cout << "call1\n";
+  return lhs < rhs;
+}
 
-  for (const auto& e : input) {
-    std::regex delimiter {","};
-    std::regex_token_iterator<std::remove_reference<decltype(e)>::type::const_iterator> start {e.cbegin(), e.cend(), delimiter, -1},
-                                                                                        end {};
-    for (;start != end; ++start)
-      std::cout << *start << ' ';
-    std::cout << std::endl;
-  }
+template<typename T>
+bool func(const T& lhs, const T& rhs) {
+  std::cout << "call2\n";
+  return lhs < rhs;
+}
+
+template<>
+bool func(const int& lhs, const int& rhs) {
+  std::cout << "call3\n";
+  return lhs < rhs;
+}
+
+int main(int argc, char** argv) {
+  constexpr int a1 {0}, a2 {1};
+  func(a1, a2);
+
+  constexpr double b1 {0.1}, b2 {3.1};
+  func(b1, b2);
+
+  func(a1, b2);
 }
