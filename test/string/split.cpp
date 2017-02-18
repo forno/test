95586f8cc1fd35ed057d21d30df64cee14154ab2
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-19 02:13:41 +0900

    Add test case

--- test/string/split.cpp
+++ test/string/split.cpp
@@ -56,3 +56,23 @@ TEST(StringLiteralTest, HandleNormalInput)
   std::vector<std::string> ans {"a", "b", "c"};
   ASSERT_EQ(result, ans);
 }
+
+TEST(StringPointerTest, HandlesNullInput)
+{
+  char base[] {'\0'};
+  auto delimiter = "";
+  auto result = xmaho::string::split(base, delimiter);
+  ASSERT_EQ(result, std::vector<std::string>{""});
+}
+
+TEST(StringPointerTest, HandleNormalInput)
+{
+  std::string base_context("a,b,c");
+  char base[6];
+  std::copy(base_context.cbegin(), base_context.cend(), base);
+  base[base_context.size()] = '\0';
+  auto delimiter = ",";
+  auto result = xmaho::string::split(base, delimiter);
+  std::vector<std::string> ans {"a", "b", "c"};
+  ASSERT_EQ(result, ans);
+}
