e39de264ebed1db72714a6852c3ed65ed10ae14d
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-19 04:36:22 +0900

    Testing: NG/pointer reference with array

--- main.cpp
+++ main.cpp
@@ -4,30 +4,14 @@
 #include <type_traits>
 #include <vector>
 
-template<typename T, typename S>
-bool func(const T& lhs, const S& rhs) {
-  std::cout << "call1\n";
-  return lhs < rhs;
-}
-
-template<typename T>
-bool func(const T& lhs, const T& rhs) {
-  std::cout << "call2\n";
-  return lhs < rhs;
-}
-
-template<>
-bool func(const int& lhs, const int& rhs) {
-  std::cout << "call3\n";
-  return lhs < rhs;
+template<typename CharT>
+void func(const CharT*& input)
+{
+  std::cout << input << std::endl;
 }
 
 int main(int argc, char** argv) {
-  constexpr int a1 {0}, a2 {1};
-  func(a1, a2);
-
-  constexpr double b1 {0.1}, b2 {3.1};
-  func(b1, b2);
-
-  func(a1, b2);
+  func("hoge");
+  auto buf = "nyan";
+  func(buf);
 }
