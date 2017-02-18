e42486576ea423134d5546e684c70f0b8c8c9875
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-19 02:35:14 +0900

    Add test for String literal (a.k.a const char[])

--- test/string/split.cpp
+++ test/string/split.cpp
@@ -57,6 +57,23 @@ TEST(StringPointerTest, HandleNormalInput)
   ASSERT_EQ(result, ans);
 }
 
+TEST(StringLiteralTest, HandlesNullInput)
+{
+  const char base[] {""};
+  const char delimiter[] {""};
+  auto result = xmaho::string::split(base, delimiter);
+  ASSERT_EQ(result, std::vector<std::string>{""});
+}
+
+TEST(StringLiteralTest, HandleNormalInput)
+{
+  const char base[] {"a,b,c"};
+  const char delimiter[] {","};
+  auto result = xmaho::string::split(base, delimiter);
+  std::vector<std::string> ans {"a", "b", "c"};
+  ASSERT_EQ(result, ans);
+}
+
 TEST(StringLiteralTest, HandlesNoConstNullInput)
 {
   char base[] {'\0'};
