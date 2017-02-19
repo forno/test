99269b63d2892b0a1f8aa6f8d60f8b156677f316
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-19 21:28:24 +0900

    Test: T* checker

--- main.cpp
+++ main.cpp
@@ -1,17 +1,8 @@
-#include <iostream>
-#include <regex>
-#include <string>
-#include <type_traits>
-#include <vector>
-
-template<typename CharT>
-void func(const CharT*& input)
+template<typename T>
+[[deprecated]] void func(T*)
 {
-  std::cout << input << std::endl;
 }
 
 int main(int argc, char** argv) {
-  func("hoge");
-  auto buf = "nyan";
-  func(buf);
+  func("");
 }
