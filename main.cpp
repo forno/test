1e7a8fd840682ea6f2525d364edf6fe3ffdafd31
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-19 21:53:48 +0900

    Test: alias of embed array cannot make by new

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
+#include <iostream>
+
+using Int3Array = int[3];
+using Int3ArrayP = Int3Array*;
+
 template<typename T>
-[[deprecated]] void func(T*)
+void view_type(T);
+
+template<typename S>
+S& operator<<(S& stream, const Int3Array& value)
 {
+  stream << value[0] << value[1] << value[2] << std::endl;
+  return stream;
 }
 
 int main(int argc, char** argv) {
-  func("");
+  Int3Array a {1, 2, 3};
+  Int3ArrayP p {&a}; // OK
+  Int3ArrayP p {new Int3Array}; // Error
 }
