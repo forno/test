be94599cebdb7b6bf54f5b13d115b426722d931c
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-22 19:13:31 +0900

    Add test case for addition and subtraction

--- test/math/residue_system.cpp
+++ test/math/residue_system.cpp
@@ -27,6 +27,20 @@ TEST(BaseModulo1Test, OverModuloNumber)
   ASSERT_EQ(residue_number, 0u);
 }
 
+TEST(BaseModulo1Test, Addition)
+{
+  xmaho::math::residue_system<1> num1 {};
+  xmaho::math::residue_system<1> num2 {};
+  ASSERT_EQ(num1 + num2, xmaho::math::residue_system<1>{});
+}
+
+TEST(BaseModulo1Test, Subtraction)
+{
+  xmaho::math::residue_system<1> num1 {};
+  xmaho::math::residue_system<1> num2 {};
+  ASSERT_EQ(num1 - num2, xmaho::math::residue_system<1>{});
+}
+
 TEST(BaseModulo2Test, NullInput)
 {
   xmaho::math::residue_system<2> residue_number {};
@@ -50,4 +64,25 @@ TEST(BaseModulo2Test, OverModuloNumber)
 {
   xmaho::math::residue_system<2> residue_number {3};
   ASSERT_EQ(residue_number, 1u);
+}
+
+TEST(BaseModulo2Test, NormalAddition)
+{
+  xmaho::math::residue_system<2> num1 {};
+  xmaho::math::residue_system<2> num2 {1};
+  ASSERT_EQ(num1 + num2, xmaho::math::residue_system<2>{1});
+}
+
+TEST(BaseModulo2Test, OverAddition)
+{
+  xmaho::math::residue_system<2> num1 {1};
+  xmaho::math::residue_system<2> num2 {1};
+  ASSERT_EQ(num1 + num2, xmaho::math::residue_system<2>{});
+}
+
+TEST(BaseModulo2Test, Subtraction)
+{
+  xmaho::math::residue_system<2> num1 {1};
+  xmaho::math::residue_system<2> num2 {1};
+  ASSERT_EQ(num1 - num2, xmaho::math::residue_system<2>{});
 }
