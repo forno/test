860b5e9e638172ae5f93482ce533afb64f07149b
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-28 23:52:58 +0900

    Show string::const_iterator task

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 template<typename T> class show_type;
 
 int main(int argc, char** argv) {
-  show_type<xmaho::traits::Value_type<const char*>>{};
+  show_type<xmaho::traits::Value_type<std::string::const_iterator>>{};
+  show_type<std::string::const_iterator>{};
 }
