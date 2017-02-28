1f048d20c46502be92f52c632fc914b09a62a3a4
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-28 21:45:48 +0900

    String difference: normal or iterator construct

--- main.cpp
+++ main.cpp
@@ -1,22 +1,11 @@
-#include <tuple>
+#include <iostream>
+#include <iterator>
+#include <string>
 
 #include <xmaho/xmaho.hpp>
 
-
-template<typename T>
-auto f(T v)
-{
-  return std::tuple<T>{v};
-}
-
-template<typename T, typename... Rests>
-auto f(T v, Rests... rests)
-{
-  if (v)
-    return std::tuple_cat(std::tuple<T>{v}, f(rests...));
-  return f(rests...);
-}
-
 int main(int argc, char** argv) {
-  f(5, 3.3, 2, 31, 'v') != f(3, 2, 3, 'a', 23.4);
+  const char a[] {'a', 'v', '\0'};
+  std::cout << std::string {std::begin(a), std::end(a)}.size() <<
+    ':' << std::string {a}.size();
 }
