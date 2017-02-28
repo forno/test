7674bfe541db1f3b745d402f21c44383e0bd6aa4
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-02-28 20:40:57 +0900

    Change return type with template in template

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <string>
+#include <unordered_set>
+
 #include <xmaho/xmaho.hpp>
 
+template<typename OStream, template<typename...> class Container, typename... Args>
+inline OStream& operator<<(OStream& s, const Container<Args...>& c)
+{
+  for (const auto& v : c)
+    s << v;
+  return s;
+}
+
+template<template<typename...> class Container = std::vector>
+Container<std::string> foo()
+{
+  return {"hogehoge", "nyannyan"};
+}
+
 int main(int argc, char** argv) {
-  while (argc--) {
-    std::cout << *argv << std::endl;
-    ++argv;
-  }
+  std::cout << foo<std::unordered_set>();
 }
