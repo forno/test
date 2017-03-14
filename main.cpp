aa438049d56e6f1d75d506e6acfb0f040b0ca1bb
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-14 11:39:09 +0900

    Array get test

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <cstddef>
 #include <iostream>
 #include <memory>
@@ -9,29 +10,7 @@
 
 template<typename T> [[deprecated]] void show_type(T&&){};
 
-template<std::size_t N>
-struct str
-{
-  char data_[N];
-
-  template<typename String, typename T, T... Seq>
-  constexpr str(const String& arg, std::integer_sequence<T, Seq...>)
-    : data_ {static_cast<char>(arg[Seq])...}
-  {
-  }
-};
-
-template<std::size_t N>
-constexpr str<N> f(const char (&arg)[N])
-{
-  return str<N>(arg, std::make_index_sequence<N>{});
-}
-
-
 int main(int argc, char** argv) {
-  constexpr auto s {f("hoge")};
-  for (const auto& e : s.data_) {
-    std::cout << e << ' ';
-  }
-  std::cout << std::endl;
+  constexpr std::array<int, 4> a{0,1,2};
+  std::cout << std::get<2>(a) << std::get<3>(a) << std::endl;
 }
