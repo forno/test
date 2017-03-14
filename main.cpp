ae2f0c47f54aeab692fae1b99b74771f25df6033
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-14 17:57:07 +0900

    Test OK: const decltype()

--- main.cpp
+++ main.cpp
@@ -11,6 +11,9 @@
 template<typename T> [[deprecated]] void show_type(T&&){};
 
 int main(int argc, char** argv) {
-  constexpr std::array<int, 4> a{0,1,2};
-  std::cout << std::get<2>(a) << std::get<3>(a) << std::endl;
+  int a;
+  const decltype(a) b {0};
+  const decltype(b) c {0};
+  static_assert(std::is_same<const int, decltype(b)>{}, "what");
+  static_assert(std::is_same<const int, decltype(c)>{}, "what");
 }
