1628a0d26b3affc7bb03afb1cd0456032aa3f583
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-16 22:21:21 +0900

    Test slice return of valarray

--- main.cpp
+++ main.cpp
@@ -17,14 +17,15 @@ const auto f()
 {
   std::valarray<int> a(20);
   std::iota(std::begin(a), std::end(a), 0);
-  return a.apply([](int e){std::cout << e << ' '; return 0;});
+  return a[std::slice(1, 5, 2)];
 }
 
 int main(int argc, char** argv) {
   std::valarray<int> a(20);
   std::iota(std::begin(a), std::end(a), 0);
   auto b {a.apply([](int e){std::cout << e << ' '; return 0;})};
-  std::cout << "\nb: " << b[0] << std::endl;
-  const auto& c {f()};
-  std::cout << "\nc: " << c[0] << std::endl;
+  std::cout << "\nb: " << b[0] << '\n';
+  auto c {f()};
+  std::valarray<int> d {c};
+  std::cout << "\nd: " << d[0] << std::endl;
 }
