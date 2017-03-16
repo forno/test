c4b7e75ffd08d1cec6a5f1bef0fa273347cd950b
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-16 22:17:05 +0900

    Test wrong usecase of valarray's apply

--- main.cpp
+++ main.cpp
@@ -13,10 +13,18 @@
 
 template<typename T> [[deprecated]] void show_type(T&&){};
 
-constexpr auto limit {5};
+const auto f()
+{
+  std::valarray<int> a(20);
+  std::iota(std::begin(a), std::end(a), 0);
+  return a.apply([](int e){std::cout << e << ' '; return 0;});
+}
 
 int main(int argc, char** argv) {
-  std::valarray<int> a(10);
+  std::valarray<int> a(20);
   std::iota(std::begin(a), std::end(a), 0);
-  std::cout << a;
+  auto b {a.apply([](int e){std::cout << e << ' '; return 0;})};
+  std::cout << "\nb: " << b[0] << std::endl;
+  const auto& c {f()};
+  std::cout << "\nc: " << c[0] << std::endl;
 }
