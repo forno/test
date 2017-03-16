c26f1719860d81ed817e61f33680f2a074277253
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-16 19:54:20 +0900

    Test of manipulator for stream

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <array>
 #include <cstddef>
 #include <functional>
+#include <iomanip>
 #include <iostream>
 #include <memory>
 #include <stdexcept>
@@ -15,11 +16,11 @@ template<typename T> [[deprecated]] void show_type(T&&){};
 constexpr auto limit {5};
 
 int main(int argc, char** argv) {
-  std::default_random_engine rand {std::random_device{}()};
-  std::vector<int> counter(limit);
-  for (auto i {0}; i < 10000; ++i) {
-    std::uniform_int_distribution<> dist {0, limit - 1};
-    ++counter[dist(rand)];
-  }
-  std::for_each(counter.begin(), counter.end(), [](auto e){std::cout << e << ' ';});
+  std::cout << std::setfill('0') << std::setw(5) << 1 << 2 << 3 << '\n';
+  std::cout << 1 << 2 << 3 << '\n';
+  std::cout << std::setw(8) << "ho" << "ge" << '\n';
+  std::cout << "ho" << "ge" << '\n';
+  std::cout << std::setw(5);
+  std::cout << 0 << "nya" << std::endl;
+  std::cout << 0 << "nya" << std::endl;
 }
