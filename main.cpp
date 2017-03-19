39b44f0430933b2c8340032107acd729a0d66f1b
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-19 17:37:57 +0900

    Testing enum hash

--- main.cpp
+++ main.cpp
@@ -14,13 +14,29 @@
 
 template<typename T> [[deprecated]] void show_type(T&&){};
 
+enum class kind
+{
+  zero, one, two, three, four, five, six, seven, eight, nine
+};
+
+namespace std
+{
+
+template<>
+struct hash<kind>
+{
+  std::size_t operator()(kind) const noexcept;
+};
+
+}
+
+std::size_t std::hash<kind>::operator()(kind k) const noexcept
+{
+  return static_cast<underlying_type_t<kind>>(k);
+}
+
 int main(int argc, char** argv) {
-  int i {};
-  switch (i) {
-    using std::string;
-    using std::cout;
-  case 0:
-    string s {"hoge"};
-    cout << s;
-  }
+  const std::unordered_set<kind> set {
+    kind::zero, kind::one, kind::two, kind::three, kind::four, kind::five, kind::six, kind::seven, kind::eight, kind::nine
+  };
 }
