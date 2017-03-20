6fb3b620c22e3e69aa85ecde73b45d8f3f116933
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-20 15:07:15 +0900

    function check

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cstddef>
 #include <functional>
 #include <iomanip>
+#include <complex>
 #include <iostream>
 #include <iterator>
 #include <memory>
@@ -14,29 +15,8 @@
 
 template<typename T> [[deprecated]] void show_type(T&&){};
 
-enum class kind
-{
-  zero, one, two, three, four, five, six, seven, eight, nine
-};
-
-namespace std
-{
-
-template<>
-struct hash<kind>
-{
-  std::size_t operator()(kind) const noexcept;
-};
-
-}
-
-std::size_t std::hash<kind>::operator()(kind k) const noexcept
-{
-  return static_cast<underlying_type_t<kind>>(k);
-}
-
 int main(int argc, char** argv) {
-  const std::unordered_set<kind> set {
-    kind::zero, kind::one, kind::two, kind::three, kind::four, kind::five, kind::six, kind::seven, kind::eight, kind::nine
-  };
+  std::string s;
+  getline(std::cin, s);
+  std::cout << s;
 }
