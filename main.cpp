e356cc9d0239bbfbd09d8ae2ce79ef1ae6eec9e1
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-03-25 22:58:51 +0900

    Test: reverse iterator with base

--- main.cpp
+++ main.cpp
@@ -16,7 +16,11 @@
 template<typename T> [[deprecated]] void show_type(T&&){};
 
 int main(int argc, char** argv) {
+  std::ios_base::sync_with_stdio(false);
+
   std::string s;
   getline(std::cin, s);
-  std::cout << s;
+  auto it {std::find(std::make_reverse_iterator(s.end()), std::make_reverse_iterator(s.begin()), 'a').base()};
+  std::cout << std::distance(s.begin(), it) << '\n';
+  std::cout << s << '\n';
 }
