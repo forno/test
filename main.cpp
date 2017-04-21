897e639e0699776118eda81a3debd79bb2d965a7
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-04-21 19:06:20 +0900

    Clear problem

--- main.cpp
+++ main.cpp
@@ -1,13 +1,33 @@
+#include <complex>
 #include <iostream>
-#include <string>
+#include <vector>
+
+using point = std::complex<double>;
 
 int main(int argc, char** argv)
 {
   std::ios_base::sync_with_stdio(false);
 
-  std::string s;
-  std::cin >> s;
-  std::cout << s << '\n';
+  double origin_x, origin_y;
+  int inner_range, outer_range;
+  std::cin >> origin_x >> origin_y >> inner_range >> outer_range;
+  point origin {origin_x, origin_y};
+
+  int length;
+  std::cin >> length;
+  std::vector<point> points(length);
+  for (auto& e : points) {
+    double x, y;
+    std::cin >> x >> y;
+    e = point{x, y};
+  }
+
+  for (const auto& e : points) {
+    const auto local_point {e - origin};
+    const auto diff_length {std::abs(local_point)};
+    std::cout << (inner_range <= diff_length && diff_length <= outer_range ? "yes" : "no");
+    std::cout.put('\n');
+  }
 
   return 0;
 }
