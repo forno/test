8daeddebbeaf2df7248dadf683e4b99ca6bd58bd
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-07-25 09:54:57 +0900

    Test http request sender

--- main.cpp
+++ main.cpp
@@ -1,54 +1,107 @@
+//
+// sync_client.cpp
+// ~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2011 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
 #include <iostream>
+#include <istream>
+#include <ostream>
 #include <string>
-#include <typeinfo>
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
-#include <boost/foreach.hpp>
-#include <boost/optional.hpp>
+#include <boost/asio.hpp>
 
-using namespace boost::property_tree;
+using boost::asio::ip::tcp;
 
-int main()
+int main(int argc, char* argv[])
 {
-  ptree pt;
-  read_json("input", pt);
-
-  // Data.value
-  if (boost::optional<int> value = pt.get_optional<int>("Data.value")) {
-    std::cout << "value : " << value.get() << std::endl;
-  }
-  else {
-    std::cout << "value is nothing" << std::endl;
-  }
-
-  // Data.str
-  if (boost::optional<std::string> str = pt.get_optional<std::string>("Data.str")) {
-    std::cout << "str : " << str.get() << std::endl;
-  }
-  else {
-    std::cout << "str is nothing" << std::endl;
-  }
-
-  // Data.info
-  BOOST_FOREACH (const ptree::value_type& child, pt.get_child("Data.info")) {
-    const ptree& info = child.second;
-
-    // Data.info.id
-    if (boost::optional<int> id = info.get_optional<int>("id")) {
-      std::cout << "id : " << id.get() << std::endl;
-    }
-    else {
-      std::cout << "id is nothing" << std::endl;
+  try
+  {
+    if (argc != 3)
+    {
+      std::cout << "Usage: sync_client <server> <path>\n";
+      std::cout << "Example:\n";
+      std::cout << "  sync_client www.boost.org /LICENSE_1_0.txt\n";
+      return 1;
     }
 
-    // Data.info.name
-    if (boost::optional<std::string> name = info.get_optional<std::string>("name")) {
-      std::cout << "name : " << name.get() << std::endl;
+    boost::asio::io_service io_service;
+
+    // Get a list of endpoints corresponding to the server name.
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query(argv[1], "http");
+    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+    // Try each endpoint until we successfully establish a connection.
+    tcp::socket socket(io_service);
+    boost::asio::connect(socket, endpoint_iterator);
+
+    // Form the request. We specify the "Connection: close" header so that the
+    // server will close the socket after transmitting the response. This will
+    // allow us to treat all data up until the EOF as the content.
+    boost::asio::streambuf request;
+    std::ostream request_stream(&request);
+    request_stream << "GET " << argv[2] << " HTTP/1.0\r\n";
+    request_stream << "Host: " << argv[1] << "\r\n";
+    request_stream << "Accept: */*\r\n";
+    request_stream << "Connection: close\r\n\r\n";
+
+    // Send the request.
+    boost::asio::write(socket, request);
+
+    // Read the response status line. The response streambuf will automatically
+    // grow to accommodate the entire line. The growth may be limited by passing
+    // a maximum size to the streambuf constructor.
+    boost::asio::streambuf response;
+    boost::asio::read_until(socket, response, "\r\n");
+
+    // Check that response is OK.
+    std::istream response_stream(&response);
+    std::string http_version;
+    response_stream >> http_version;
+    unsigned int status_code;
+    response_stream >> status_code;
+    std::string status_message;
+    std::getline(response_stream, status_message);
+    if (!response_stream || http_version.substr(0, 5) != "HTTP/")
+    {
+      std::cout << "Invalid response\n";
+      return 1;
     }
-    else {
-      std::cout << "name is nothing" << std::endl;
+    if (status_code != 200)
+    {
+      std::cout << "Response returned with status code " << status_code << "\n";
+      return 1;
     }
+
+    // Read the response headers, which are terminated by a blank line.
+    boost::asio::read_until(socket, response, "\r\n\r\n");
+
+    // Process the response headers.
+    std::string header;
+    while (std::getline(response_stream, header) && header != "\r")
+      std::cout << header << "\n";
+    std::cout << "\n";
+
+    // Write whatever content we already have to output.
+    if (response.size() > 0)
+      std::cout << &response;
+
+    // Read until EOF, writing data to output as we go.
+    boost::system::error_code error;
+    while (boost::asio::read(socket, response,
+          boost::asio::transfer_at_least(1), error))
+      std::cout << &response;
+    if (error != boost::asio::error::eof)
+      throw boost::system::system_error(error);
+  }
+  catch (std::exception& e)
+  {
+    std::cout << "Exception: " << e.what() << "\n";
   }
 
-
+  return 0;
 }
