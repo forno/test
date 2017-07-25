4765ad41472c498aa3ded0cab5e84de2efa2b495
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-07-25 17:36:29 +0900

    Boost asio test : It complexed. maybe few's unneed

--- main.cpp
+++ main.cpp
@@ -1,13 +1,107 @@
-#include <boost/network/protocol/http/client.hpp>
+//
+// sync_client.cpp
+// ~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2011 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 
-int main()
+#include <iostream>
+#include <istream>
+#include <ostream>
+#include <string>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+int main(int argc, char* argv[])
 {
-  using namespace boost::network;
-  using namespace boost::network::http;
-  client::request request_("http://s2707-1:3000/api/v0/users");
-  request_ << header("Authorization", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfX3YiOjAsInZhbHVlIjoiWUV3OVNKZEFvV1NQVzhycnBEQ0Qza1QxaXBhQnRoWHFuUWxnZVFuYXVOQ0lqRUp1MGlFYzg1UjN3cVF1N1c1RWxHejlya0E0d0dXbWJJZmQ0RW5uUFNDVE8xd1dQWUwyMXJzU0xwNzdKc0Fnc0xmS0N5ZUQ2QTV2dDc2dERtSmd0Y2dZNmNtOW9LeUxtc2R3a2RVSVk0amx5aEMyVmhjaVNIaU5kTWN1SnlIM2tXQ2VYMnNBZnJSVnlTamZtcFZBaEJOUU5oQzNuZ0hmb3NpRzdoVHFVWnlhb3VRWWltMUFjclVMaHQ0VnpaRDUzNlVhRURUMGV2S2V0eVJ2WUluOCIsImNsaWVudF9pZCI6IjU5NzVhMjlmMjg5NmE5NDg5YTc5NzExZiIsInVzZXJfaWQiOiI1OTc1YTI5MzI4OTZhOTQ4OWE3OTcxMWUiLCJfaWQiOiI1OTc2ZmJhODRiNjRiNTEzYzZkZjRkZGEifQ.758y7VQi2RyjwvOr3rVMUHhSJGum-Mro1bE2lzwD2BM");
-  client client_;
-  client::response response_ = client_.get(request_);
-  std::string body_ = body(response_);
-  std::cout << body_;
+  try
+  {
+    if (argc != 3)
+    {
+      std::cout << "Usage: sync_client <server> <path>\n";
+      std::cout << "Example:\n";
+      std::cout << "  sync_client www.boost.org /LICENSE_1_0.txt\n";
+      return 1;
+    }
+
+    boost::asio::io_service io_service;
+
+    // Get a list of endpoints corresponding to the server name.
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query(argv[1], "http");
+    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+    // Try each endpoint until we successfully establish a connection.
+    tcp::socket socket(io_service);
+    boost::asio::connect(socket, endpoint_iterator);
+
+    // Form the request. We specify the "Connection: close" header so that the
+    // server will close the socket after transmitting the response. This will
+    // allow us to treat all data up until the EOF as the content.
+    boost::asio::streambuf request;
+    std::ostream request_stream(&request);
+    request_stream << "GET " << argv[2] << " HTTP/1.0\r\n";
+    request_stream << "Host: " << argv[1] << "\r\n";
+    request_stream << "Accept: */*\r\n";
+    request_stream << "Connection: close\r\n\r\n";
+
+    // Send the request.
+    boost::asio::write(socket, request);
+
+    // Read the response status line. The response streambuf will automatically
+    // grow to accommodate the entire line. The growth may be limited by passing
+    // a maximum size to the streambuf constructor.
+    boost::asio::streambuf response;
+    boost::asio::read_until(socket, response, "\r\n");
+
+    // Check that response is OK.
+    std::istream response_stream(&response);
+    std::string http_version;
+    response_stream >> http_version;
+    unsigned int status_code;
+    response_stream >> status_code;
+    std::string status_message;
+    std::getline(response_stream, status_message);
+    if (!response_stream || http_version.substr(0, 5) != "HTTP/")
+    {
+      std::cout << "Invalid response\n";
+      return 1;
+    }
+    if (status_code != 200)
+    {
+      std::cout << "Response returned with status code " << status_code << "\n";
+      return 1;
+    }
+
+    // Read the response headers, which are terminated by a blank line.
+    boost::asio::read_until(socket, response, "\r\n\r\n");
+
+    // Process the response headers.
+    std::string header;
+    while (std::getline(response_stream, header) && header != "\r")
+      std::cout << header << "\n";
+    std::cout << "\n";
+
+    // Write whatever content we already have to output.
+    if (response.size() > 0)
+      std::cout << &response;
+
+    // Read until EOF, writing data to output as we go.
+    boost::system::error_code error;
+    while (boost::asio::read(socket, response,
+          boost::asio::transfer_at_least(1), error))
+      std::cout << &response;
+    if (error != boost::asio::error::eof)
+      throw boost::system::system_error(error);
+  }
+  catch (std::exception& e)
+  {
+    std::cout << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
 }
