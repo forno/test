954b78a5a914c98a112b45bd8e1cf34e67253a11
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-08-07 05:04:14 +0900

    Test c++17 CMakeFiles

--- main.cpp
+++ main.cpp
@@ -1,107 +1,58 @@
-//
-// sync_client.cpp
-// ~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2017 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
+#include <chrono>
 #include <iostream>
-#include <istream>
-#include <ostream>
-#include <string>
-#include <boost/asio.hpp>
+#include <mutex>
+#include <shared_mutex>
+#include <thread>
+#include <vector>
 
-using boost::asio::ip::tcp;
-
-int main(int argc, char* argv[])
+template<typename T>
+struct Base
 {
-  try
-  {
-    if (argc != 3)
-    {
-      std::cout << "Usage: sync_client <server> <path>\n";
-      std::cout << "Example:\n";
-      std::cout << "  sync_client www.boost.org /LICENSE_1_0.txt\n";
-      return 1;
+};
+
+template<template<typename...> typename T>
+struct X
+{
+};
+
+template struct Base<int>;
+
+template struct X<Base>;
+
+int main()
+{
+  std::shared_timed_mutex count_mtx {};
+
+  auto count {0};
+
+  std::thread w {[&count, &count_mtx](){
+    for (auto i {3}; i > 0; --i) {
+      {
+        std::lock_guard<std::shared_timed_mutex> count_lock {count_mtx};
+        ++count;
+      }
+      std::this_thread::sleep_for(std::chrono::milliseconds{1});
     }
+  }};
 
-    boost::asio::io_service io_service;
+  std::mutex cout_mtx {};
 
-    // Get a list of endpoints corresponding to the server name.
-    tcp::resolver resolver(io_service);
-    tcp::resolver::query query(argv[1], "http");
-    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
-
-    // Try each endpoint until we successfully establish a connection.
-    tcp::socket socket(io_service);
-    boost::asio::connect(socket, endpoint_iterator);
-
-    // Form the request. We specify the "Connection: close" header so that the
-    // server will close the socket after transmitting the response. This will
-    // allow us to treat all data up until the EOF as the content.
-    boost::asio::streambuf request;
-    std::ostream request_stream(&request);
-    request_stream << "GET " << argv[2] << " HTTP/1.0\r\n";
-    request_stream << "Host: " << argv[1] << "\r\n";
-    request_stream << "Accept: */*\r\n";
-    request_stream << "Connection: close\r\n\r\n";
-
-    // Send the request.
-    boost::asio::write(socket, request);
-
-    // Read the response status line. The response streambuf will automatically
-    // grow to accommodate the entire line. The growth may be limited by passing
-    // a maximum size to the streambuf constructor.
-    boost::asio::streambuf response;
-    boost::asio::read_until(socket, response, "\r\n");
-
-    // Check that response is OK.
-    std::istream response_stream(&response);
-    std::string http_version;
-    response_stream >> http_version;
-    unsigned int status_code;
-    response_stream >> status_code;
-    std::string status_message;
-    std::getline(response_stream, status_message);
-    if (!response_stream || http_version.substr(0, 5) != "HTTP/")
-    {
-      std::cout << "Invalid response\n";
-      return 1;
+  auto reader {[&count, &cout_mtx, &count_mtx](){
+    for (auto i {10}; i > 0; --i) {
+      {
+        std::shared_lock<std::shared_timed_mutex> count_lock {count_mtx};
+        std::lock_guard<std::mutex> cout_lock {cout_mtx};
+        std::cout << count << '\n';
+      }
+      std::this_thread::sleep_for(std::chrono::milliseconds{1});
     }
-    if (status_code != 200)
-    {
-      std::cout << "Response returned with status code " << status_code << "\n";
-      return 1;
-    }
+  }};
 
-    // Read the response headers, which are terminated by a blank line.
-    boost::asio::read_until(socket, response, "\r\n\r\n");
+  std::vector<std::thread> r {};
+  for (auto i {3}; i > 0; --i)
+    r.emplace_back(reader);
 
-    // Process the response headers.
-    std::string header;
-    while (std::getline(response_stream, header) && header != "\r")
-      std::cout << header << "\n";
-    std::cout << "\n";
-
-    // Write whatever content we already have to output.
-    if (response.size() > 0)
-      std::cout << "Too many header : " << &response;
-
-    // Read until EOF, writing data to output as we go.
-    boost::system::error_code error;
-    while (boost::asio::read(socket, response,
-          boost::asio::transfer_at_least(1), error))
-      std::cout << "To last : " << &response;
-    if (error != boost::asio::error::eof)
-      throw boost::system::system_error(error);
-  }
-  catch (std::exception& e)
-  {
-    std::cout << "Exception: " << e.what() << "\n";
-  }
-
-  return 0;
+  w.join();
+  for (auto& e : r)
+    e.join();
 }
