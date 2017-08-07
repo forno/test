ae5a79224b1a3e4e416b349a10f9ebf384d6c807
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-08-07 15:06:15 +0900

    Test for xmaho::ability_bank::Connector on system

--- main.cpp
+++ main.cpp
@@ -1,58 +1,10 @@
-#include <chrono>
 #include <iostream>
-#include <mutex>
-#include <shared_mutex>
-#include <thread>
-#include <vector>
 
-template<typename T>
-struct Base
+#include <xmaho/ability_bank/connector.hpp>
+
+int main(int argc, char** argv)
 {
-};
-
-template<template<typename...> typename T>
-struct X
-{
-};
-
-template struct Base<int>;
-
-template struct X<Base>;
-
-int main()
-{
-  std::shared_timed_mutex count_mtx {};
-
-  auto count {0};
-
-  std::thread w {[&count, &count_mtx](){
-    for (auto i {3}; i > 0; --i) {
-      {
-        std::lock_guard<std::shared_timed_mutex> count_lock {count_mtx};
-        ++count;
-      }
-      std::this_thread::sleep_for(std::chrono::milliseconds{1});
-    }
-  }};
-
-  std::mutex cout_mtx {};
-
-  auto reader {[&count, &cout_mtx, &count_mtx](){
-    for (auto i {10}; i > 0; --i) {
-      {
-        std::shared_lock<std::shared_timed_mutex> count_lock {count_mtx};
-        std::lock_guard<std::mutex> cout_lock {cout_mtx};
-        std::cout << count << '\n';
-      }
-      std::this_thread::sleep_for(std::chrono::milliseconds{1});
-    }
-  }};
-
-  std::vector<std::thread> r {};
-  for (auto i {3}; i > 0; --i)
-    r.emplace_back(reader);
-
-  w.join();
-  for (auto& e : r)
-    e.join();
+  xmaho::ability_bank::Connector connector {"localhost", "80", "/"};
+  std::cout << connector("") << "\n\n";
+  std::cout << connector("random-pick.html") << "\n\n";
 }
