09a52b4532dca7b00395cc936fd3679578563bfe
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-08-29 19:22:33 +0900

    Eigen print test

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <iterator>
+
 #include <Eigen/Dense>
 
 enum class EC
@@ -11,8 +14,18 @@ class C
 
 };
 
+std::ostream& operator<<(std::ostream& os, const EC& v) {
+  os << static_cast<int>(v);
+  return os;
+}
+
 int main()
 {
-  Eigen::Matrix<EC, 10, 10> field1;
-  Eigen::Matrix<C, 10, 10> field2;
+  Eigen::Matrix<EC, 10, 10> field1 {};
+  Eigen::Matrix<C, 10, 10> field2 {};
+
+  std::copy(field1.data(),
+            field1.data() + field1.size(),
+            std::ostream_iterator<EC>(std::cout, " "));
+  std::cout.put('\n');
 }
