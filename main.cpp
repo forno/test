226392636eb98dc6ba07b7c90948fba35f204d5a
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-09-01 21:55:38 +0900

    Template perfect overload: perfect forward

--- main.cpp
+++ main.cpp
@@ -1,44 +1,66 @@
-#include <initializer_list>
 #include <iostream>
-#include <type_traits>
 #include <utility>
-#include <valarray>
-#include <vector>
 
-class MyVector
+template<typename T>
+void func(T&&)
 {
-public:
-  template<typename... Args>
-    MyVector(Args&&... args)
-    : array_(std::forward<Args>(args)...)
-  {
-  }
+  std::cout << "default\n";
+}
 
-  MyVector(std::initializer_list<int> init)
-    : array_(init)
-  {
-  }
-
-  template<typename... Args>
-  auto operator[](Args&&... args) -> decltype(std::declval<std::valarray<int>>().operator[](std::forward<Args>(args)...))
-  {
-    return array_.operator[](std::forward<Args>(args)...);
-  }
-
-  decltype(auto) operator[](const std::vector<int>& i)
-  {
-    std::cout << "my extends\n";
-    return array_[i[0]];
-  }
-
-private:
-  std::valarray<int> array_;
-};
-
-
-int main()
+const int& func(const int& v)
 {
-  MyVector v(10);
-  std::vector<int> hoge {2};
-  std::cout << v[hoge] << '\n';
+  std::cout << "const int&\n";
+  return v;
+}
+
+int& func(int& v)
+{
+  std::cout << "int&\n";
+  return v;
+}
+
+int&& func(int&& v)
+{
+  std::cout << "int&&\n";
+  return std::move(v);
+}
+
+volatile const int& func(volatile const int& v)
+{
+  std::cout << "volatile const int&\n";
+  return v;
+}
+
+volatile int& func(volatile int& v)
+{
+  std::cout << "volatile int&\n";
+  return v;
+}
+
+volatile int&& func(volatile int&& v)
+{
+  std::cout << "volatile int&&\n";
+  return std::move(v);
+}
+
+void call()
+{
+}
+
+template<typename Head, typename... Tail>
+void call(Head&& value, Tail&&... rect)
+{
+  func(std::forward<Head>(value));
+  call(std::forward<Tail>(rect)...);
+}
+
+int main(int argc, char** argv)
+{
+  int i {0};
+  const int ci {0};
+  volatile int vi {0};
+  volatile const int vci {0};
+  volatile int rvi {0};
+
+  call(i, ci, 0, vi, vci, std::move(rvi));
 }
