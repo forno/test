d96b22c0a685098300ba2a26383e0218e0eb8222
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-09-01 11:19:40 +0900

    Can make with multi move

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
+#include <cstddef>
 #include <iostream>
+#include <limits>
 #include <utility>
-
-void func(std::pair<int, int> p)
-{
-  std::cout << p.first << " , " << p.second << '\n';
-}
+#include <valarray>
 
 int main(int argc, char** argv)
 {
-  func({1, 5});
+  std::cout << "max size: " << std::numeric_limits<std::size_t>::max() << std::endl;
+  std::valarray<int> v1(std::numeric_limits<std::size_t>::max() / 18446744073ul);
+  std::cout << "v1 is created" << std::endl;
+  std::valarray<int> v2 {std::move(v1)};
+  std::cout << "v2 is created" << std::endl;
+  std::valarray<int> v3 {std::move(v2)};
+  std::cout << "v3 is created" << std::endl;
 }
