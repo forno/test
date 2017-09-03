ed104d7b195b62f0bc2598dca156dfa6497af3dd
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-09-04 01:12:13 +0900

    Check private extend class

--- main.cpp
+++ main.cpp
@@ -1,31 +1,37 @@
-#include <algorithm>
-#include <array>
 #include <iostream>
-#include <iterator>
-#include <random>
+#include <type_traits>
 #include <valarray>
+
+template<typename T>
+class valmatrix : private std::valarray<T>
+{
+public:
+  //! Value type of valarray
+  using std::valarray<T>::value_type;
+  //! Size type for access to values.
+  using size_type = std::size_t;
+  //! Specific position type for two dimention.
+  using position_type = std::pair<size_type, size_type>;
+
+  /**
+   * @brief Default contructor.
+   */
+  valmatrix() = default;
+
+  //! Define default copy assign operator to avoid forward
+  valmatrix& operator=(const valmatrix&) = default;
+  //! Define default move assign operator to avoid forward
+  valmatrix& operator=(valmatrix&&) = default;
+
+  using std::valarray<T>::operator=;
+};
 
 int main(int argc, char** argv)
 {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  std::random_device seed_gen;
-  std::array<std::seed_seq::result_type, std::mt19937_64::state_size> seed_data;
-  std::generate(seed_data.begin(), seed_data.end(), [&seed_gen]{return seed_gen();});
-
-  std::seed_seq seq(seed_data.begin(), seed_data.end());
-  std::mt19937_64 rand {seq};
-  std::normal_distribution<> dist {0, 1};
-
-  std::cout << "make values" << std::endl;
-  std::valarray<double> population(100000000ul); // 500000000ul ok
-  for (auto& e : population)
-    e = dist(rand);
-
-  std::cout << "sample values" << std::endl;
-  std::valarray<double> sample(10000ul);
-  std::sample(std::begin(population), std::end(population), std::begin(sample), sample.size(), rand);
-
-  std::copy(std::begin(sample), std::end(sample), std::ostream_iterator<double>{std::cout, "\n"});
+  valmatrix<int> vec;
+  static_assert(std::is_same<decltype(vec = std::valarray<int>{}), std::valarray<int>&>::value, "super type...");
+  static_assert(std::is_same<valmatrix<int>::value_type, std::valarray<int>::value_type>::value, "only using value_type");
 }
