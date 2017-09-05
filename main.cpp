b00a8f78365199194cba459e4b4b5f50dde9844c
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-09-05 22:44:54 +0900

    Cmake list test and private extention

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include <iostream>
-#include <type_traits>
 #include <valarray>
+
+class C : std::valarray<int>
+{
+public:
+  using std::valarray<int>::valarray;
+  using std::valarray<int>::operator+;
+};
 
 int main(int argc, char** argv)
 {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-
-  static_assert(std::is_same<int&, decltype(std::declval<int&>())>{},
-                "declval with lvalue reference");
+  C o {0, 1};
+  std::valarray<int> test {+o};
+  // C test {+o}; // compile error
 }
