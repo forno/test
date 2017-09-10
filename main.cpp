6d4e1dd1ff2f215c523bde422f1062c8e4c0aff5
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-09-11 00:05:35 +0900

    Test construct int with non-explicit call

--- main.cpp
+++ main.cpp
@@ -1,33 +1,13 @@
 #include <iostream>
-#include <valarray>
 
 using namespace std;
 
-class C : private valarray<int>
+void func(int a, int b)
 {
-public:
-  using valarray<int>::valarray;
-
-  auto begin() {
-    return std::begin(static_cast<valarray<int>>(*this));
-  }
-
-  auto end() {
-    return std::begin(static_cast<valarray<int>>(*this));
-  }
-};
+  cout << a << ' ' << b << '\n';
+}
 
 int main(int argc, char** argv)
 {
-  {
-    C array {2, 4, 5, 8, 10};
-    for (auto e : array)
-      std::cout << e << '\n';
-  }
-
-  {
-    valarray<int> array {2, 4, 5, 8, 10};
-    for (auto e : array)
-      std::cout << e << '\n';
-  }
+  func({}, {});
 }
