685191739c08bc2a3c3064b4ff7e2adf8091de89
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-11-27 15:37:44 +0900

    Test: Private extends cast

--- main.cpp
+++ main.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
 #include <string>
+#include <utility>
+
+class Base
+{
+};
+
+class Deriverd
+  : public Base
+{
+};
+
+class DeriverdPrivate
+  : Base
+{
+public:
+  Base& as_base()
+  {
+    return static_cast<Base&>(*this);
+  }
+};
 
 int main(int argc, char** argv)
 {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  std::string a, b;
-  std::cin >> a >> b;
-  if (a.back() != b.front()) {
-    std::cout << "NG\n";
-    return 0;
-  }
-  if (b.back() == 'n') {
-    std::cout << "NG\n";
-    return 0;
-  }
-  std::cout << "OK\n";
+  Deriverd deriverd {};
+  Base& base_deriverd {static_cast<Base&>(deriverd)};
+
+  DeriverdPrivate deriverd_private {};
+  Base& base_deriverd_private {deriverd_private.as_base()};
 }
