913d88f25e59d483deb6a5f66e660259471ac1b5
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-12-10 01:34:04 +0900

    Test of line notify

--- main.cpp
+++ main.cpp
@@ -1,22 +1,16 @@
-#include <cmath>
-#include <iostream>
-#include <limits>
-#include <random>
+#include <cstdlib>
+#include <sstream>
+
+namespace
+{
+
+constexpr auto token {"xtv6U9xiWGDTLpp98eThGM5bDg5pywNgJfJE20Vcacd"};
+
+}
 
 int main(int argc, char** argv)
 {
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr);
-
-  std::uniform_int_distribution<> dist {-static_cast<int>(std::pow(std::abs(std::numeric_limits<int>::min() + 1), 1. / 5)), static_cast<int>(std::pow(std::numeric_limits<int>::max(), 1. / 5))};
-  std::default_random_engine rand {std::random_device{}()};
-
-  auto a {dist(rand)};
-  auto b {dist(rand)};
-
-  std::cout << "abs(min) == " << std::abs(std::numeric_limits<int>::min()) << '\n';
-  std::cout << "[min max]             == [" << -static_cast<int>(std::pow(std::abs(std::numeric_limits<int>::min() + 1), 1. / 5)) << ' ' << static_cast<int>(std::pow(std::numeric_limits<int>::max(), 1. / 5)) << "]\n"
-            << "[a b]                 == [" << a << ' ' << b << "]\n"
-            << "[a*a*a b*b*b]         == [" << a*a*a << ' ' << b*b*b << "]\n"
-            << "[pow(a, 3) pow(b, 3)] == [" << std::pow(a, 3) << ' ' << std::pow(b, 3) << "]\n";
+  std::ostringstream oss {};
+  oss << "curl -X POST -H 'Authorization: Bearer " << token << "' -F 'message=foobar' https://notify-api.line.me/api/notify";
+  std::system(oss.str().c_str());
 }
