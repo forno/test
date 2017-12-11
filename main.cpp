50dcc3ba8a7fc3bdf81dbfae057665ff41223d9a
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-12-11 19:45:07 +0900

    LINE notify practice

--- main.cpp
+++ main.cpp
@@ -13,6 +13,6 @@ constexpr auto token {
 int main(int argc, char** argv)
 {
   std::ostringstream oss {};
-  oss << "curl -X POST -H 'Authorization: Bearer " << token << "' -F 'message=foobar' https://notify-api.line.me/api/notify";
+  oss << "curl -X POST -H 'Authorization: Bearer " << token << "' -F 'message=Studio daemon tester' https://notify-api.line.me/api/notify";
   std::system(oss.str().c_str());
 }
