9569d23a3acd6bb856a870a91bd38dd0d34ed970
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-12-16 17:03:15 +0900

    Test valarray apply return function. maybe wrong

--- main.cpp
+++ main.cpp
@@ -1,26 +1,17 @@
 #include <iostream>
 #include <unordered_map>
+#include <valarray>
+
+template<typename T>
+auto f(const std::valarray<T>& v)
+{
+  return v.apply([](const auto& e){std::cout << e << '\n'; return e;});
+}
 
 int main(int, char**)
 {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  const std::unordered_map<int, std::string> month_to_str {
-    {1, "January"},
-    {2, "February"},
-    {3, "March"},
-    {4, "April"},
-    {5, "May"},
-    {6, "June"},
-    {7, "July"},
-    {8, "August"},
-    {9, "September"},
-    {10, "October"},
-    {11, "November"},
-    {12, "December"}
-  };
-
-  const auto month {[]{int v; std::cin >> v; return v;}()};
-  std::cout << month_to_str.at(month) << '\n';
+  f(std::valarray<int>{1, 3, 5, 7, 9})[3];
 }
