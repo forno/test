bc70551e8e13644f83ab237ae6bd4d93faaeacd6
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-12-19 22:36:41 +0900

    WIP paiza code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
+#include <algorithm>
+#include <cstddef>
 #include <iostream>
-#include <unordered_map>
-#include <valarray>
+#include <istream>
+#include <string>
+#include <vector>
+
+namespace
+{
 
 template<typename T>
-auto f(const std::valarray<T>& v)
+T get_value(std::istream& is)
 {
-  return v.apply([](const auto& e){std::cout << e << '\n'; return e;});
+  T value;
+  is >> value;
+  return value;
+}
+
 }
 
 int main(int, char**)
@@ -13,5 +23,15 @@ int main(int, char**)
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  f(std::valarray<int>{1, 3, 5, 7, 9})[3];
+  const auto target {get_value<std::string>(std::cin)};
+  const auto log([s = get_value<std::size_t>(std::cin)]{
+    std::vector<std::string> v(s);
+    for (auto& e : v)
+      std::cin >> e;
+    return v;
+  }());
+
+  const auto find_it {std::find(std::begin(log), std::end(log), target)};
+  using namespace std::literals::string_literals;
+  std::cout << (find_it == std::end(log) ? "NG\n"s : std::to_string(std::distance(std::begin(log), std::end(log)))) << '\n';
 }
