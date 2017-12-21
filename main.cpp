9bdbc55b3ef72b591881aa3ba1d94b74cd1dde38
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-12-21 21:38:54 +0900

    Complate sparql request from GET

--- main.cpp
+++ main.cpp
@@ -1,37 +1,41 @@
-#include <algorithm>
 #include <cstddef>
-#include <iostream>
-#include <istream>
+#include <cstdlib>
+#include <cstring>
 #include <string>
-#include <vector>
+#include <iostream>
 
-namespace
+#include <boost/asio.hpp>
+
+constexpr auto host {"sparql.crssnky.xyz"};
+constexpr auto sparql_endpoint {"/spql/imas/query"};
+
+int main()
 {
+  using boost::asio::ip::tcp;
+  try {
+    boost::asio::io_context io_context;
+    tcp::socket s(io_context);
+    boost::asio::connect(s, tcp::resolver{io_context}.resolve(host, "http"));
 
-template<typename T>
-T get_value(std::istream& is)
-{
-  T value;
-  is >> value;
-  return value;
-}
+    const std::string query {R"(query=PREFIX%20schema%3A%20<http%3A%2F%2Fschema.org%2F>%0APREFIX%20imas%3A%20<https%3A%2F%2Fsparql.crssnky.xyz%2Fimasrdf%2FURIs%2Fimas-schema.ttl%23>%0ASELECT%20%3Fo%20%3Fh%0AWHERE%20%7B%0A%20%20%3Fs%20schema%3Aname%7Cschema%3AalternateName%20%3Fo%3B%0A%20%20%20%20%20schema%3Aheight%20%3Fh.%0A%7Dorder%20by(%3Fh))"};
+    boost::asio::streambuf request;
+    std::ostream req {&request};
+    req << "GET " << sparql_endpoint << '?' << query << " HTTP/1.1\r\n" <<
+           "Host: " << host << "\r\n" <<
+           "Accept: application/sparql-results+json\r\n\r\n";
 
-}
+    boost::asio::write(s, request);
 
-int main(int, char**)
-{
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(nullptr);
+    boost::asio::streambuf res;
+    boost::system::error_code ec;
+    boost::asio::read(s, res, ec);
 
-  const auto target {get_value<std::string>(std::cin)};
-  const auto log([s = get_value<std::size_t>(std::cin)]{
-    std::vector<std::string> v(s);
-    for (auto& e : v)
-      std::cin >> e;
-    return v;
-  }());
+    if (ec != boost::asio::error::eof)
+      throw std::runtime_error{"xmaho::ability_bank::Connector: Network error"};
 
-  const auto find_it {std::find(std::begin(log), std::end(log), target)};
-  using namespace std::literals::string_literals;
-  std::cout << (find_it == std::end(log) ? "NG\n"s : std::to_string(std::distance(std::begin(log), std::end(log)))) << '\n';
+    std::string receive_data {boost::asio::buffer_cast<const char*>(res.data())};
+    std::cout << receive_data << '\n';
+  } catch (std::exception& e) {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
 }
