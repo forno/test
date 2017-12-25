c677f4fe384d8e46c2f5b4bfe0da0f700d799fdb
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-12-26 02:42:06 +0900

    OK!! TLS 1.2 connection

--- main.cpp
+++ main.cpp
@@ -1,74 +1,60 @@
-//
-// blocking_tcp_echo_server.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2017 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
+#include <cstddef>
 #include <cstdlib>
+#include <cstring>
+#include <string>
 #include <iostream>
-#include <thread>
-#include <utility>
+
 #include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
 
-using boost::asio::ip::tcp;
+constexpr auto host {"sparql.crssnky.xyz"};
+constexpr auto sparql_endpoint {"/spql/imas/query"};
 
-const int max_length = 1024;
-
-void session(tcp::socket sock)
+int main()
 {
-  try
-  {
-    for (;;)
-    {
-      char data[max_length];
+  try {
+    boost::asio::io_context io_context {};
+    boost::asio::ssl::context ssl_context {boost::asio::ssl::context::tlsv12};
+    ssl_context.set_default_verify_paths();
+    boost::asio::ssl::stream<boost::asio::ip::tcp::socket> ssl_socket {io_context, ssl_context};
+    ssl_socket.set_verify_mode(boost::asio::ssl::verify_peer);
+    boost::asio::connect(ssl_socket.lowest_layer(), boost::asio::ip::tcp::resolver{io_context}.resolve(host, "https"));
+    ssl_socket.handshake(boost::asio::ssl::stream_base::client);
 
-      boost::system::error_code error;
-      size_t length = sock.read_some(boost::asio::buffer(data), error);
-      if (error == boost::asio::error::eof)
-        break; // Connection closed cleanly by peer.
-      else if (error)
-        throw boost::system::system_error(error); // Some other error.
+    const std::string query {R"(PREFIX schema: <http://schema.org/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX imas: <https://sparql.crssnky.xyz/imasrdf/URIs/imas-schema.ttl#>
+PREFIX imasrdf: <https://sparql.crssnky.xyz/imasrdf/RDFs/detail/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX math: <http://www.w3.org/2005/xpath-functions/math#>
 
-      boost::asio::write(sock, boost::asio::buffer(data, length));
-    }
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception in thread: " << e.what() << "\n";
-  }
-}
+SELECT ?n ?p
+WHERE {
+  ?s schema:name|schema:alternateName|imas:nameKana ?o;
+     filter(regex(str(?o),"双葉杏")).
+  ?s ?n ?p;
+})"};
+    boost::asio::streambuf request;
+    std::ostream req {&request};
+    req << "POST " << sparql_endpoint << " HTTP/1.1\r\n" <<
+           "Host: " << host << "\r\n" <<
+           "Content-Type: application/sparql-query; charset=utf-8\r\n" <<
+           "Content-Length: " << query.size() << "\r\n" <<
+           "Accept: application/sparql-results+json\r\n\r\n" <<
+           query;
 
-void server(boost::asio::io_context& io_context, unsigned short port)
-{
-  tcp::acceptor a(io_context, tcp::endpoint(tcp::v4(), port));
-  for (;;)
-  {
-    std::thread(session, a.accept()).detach();
-  }
-}
+    boost::asio::write(ssl_socket, request);
 
-int main(int argc, char* argv[])
-{
-  try
-  {
-    if (argc != 2)
-    {
-      std::cerr << "Usage: blocking_tcp_echo_server <port>\n";
-      return 1;
-    }
+    boost::asio::streambuf res;
+    boost::system::error_code ec;
+    boost::asio::read(ssl_socket, res, ec);
 
-    boost::asio::io_context io_context;
+    if (ec != boost::asio::error::eof)
+      throw std::runtime_error{"xmaho::ability_bank::Connector: Network error"};
 
-    server(io_context, std::atoi(argv[1]));
-  }
-  catch (std::exception& e)
-  {
+    std::string receive_data {boost::asio::buffer_cast<const char*>(res.data())};
+    std::cout << receive_data << '\n';
+  } catch (std::exception& e) {
     std::cerr << "Exception: " << e.what() << "\n";
   }
-
-  return 0;
 }
