276e6d8d39f2068f953b4ad48f284e35b14162ba
Doi Yusuke <o111027y@mail.kyutech.jp>
2018-01-01 17:51:28 +0900

    string_view null construct on default arguments

--- main.cpp
+++ main.cpp
@@ -5,16 +5,32 @@
 #include <iterator>
 #include <sstream>
 #include <string>
+#include <string_view>
 #include <unordered_set>
+#include <unordered_map>
 #include <valarray>
 #include <vector>
+
+void func(std::string_view = {});
+
+void func(std::string_view)
+{
+}
+
+class c
+{
+public:
+  std::unordered_map<std::string, std::string> f;
+  std::string a;
+  std::string b;
+  std::string c;
+  std::string d;
+};
 
 int main(int, char**)
 {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  std::cout << static_cast<std::ostringstream&>(std::ostringstream{} << "one" << "two" << "three").str() << '\n';
-  std::ostringstream oss{};
-  std::cout << static_cast<std::ostringstream&>(oss << "one" << "two" << "three").str() << '\n';
+  func();
 }
