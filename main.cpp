5bba00d9171435b24036b630660dac43c8e94c32
Doi Yusuke <o111027y@mail.kyutech.jp>
2018-01-13 01:41:29 +0900

    Test compile error : constexpr value

--- main.cpp
+++ main.cpp
@@ -12,62 +12,17 @@
 #include <valarray>
 #include <vector>
 
-class C
-{
-public:
-  C()
-    : value_ {"default"}
-  {
-    std::cout << "C::C : default constructer " << value_ << '\n';
-  }
+template<typename T>
+constexpr T value;
 
-  C(std::string_view value)
-    : value_ {std::cbegin(value), std::cend(value)}
-  {
-    std::cout << "C::C : default constructer " << value_ << '\n';
-  }
-
-  C(const C&)
-  {
-    std::cout << "C::C : copy constructer " << value_ << '\n';
-  }
-
-  C(C&&)
-  {
-    std::cout << "C::C : move constructer " << value_ << '\n';
-  }
-
-  C& operator=(const C&)
-  {
-    std::cout << "C::C : copy assign " << value_ << '\n';
-    return *this;
-  }
-
-  C& operator=(C&&)
-  {
-    std::cout << "C::C : move assign " << value_ << '\n';
-    return *this;
-  }
-
-  template<typename... Args>
-  C& operator()(Args...)
-  {
-    return *this;
-  }
-
-  ~C()
-  {
-    std::cout << "C::~C : destructer " << value_ << '\n';
-  }
-
-private:
-  std::string value_;
-};
+template<>
+constexpr auto value<int> {0};
 
 int main(int, char**)
 {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  C{"wide"}(C{"first argument"})(C{"second argument1"}, C{"second argument2"})(C{"third argument"});
+  std::cout << value<int>;
+  std::cout << value<char>;
 }
