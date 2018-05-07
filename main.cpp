9a67fc3fa4f3509a85195e224bf1eefa30609976
Doi Yusuke <o111027y@mail.kyutech.jp>
2018-05-07 14:36:56 +0900

    Test: Implementation of big integer

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <algorithm>
+#include <bitset>
+#include <cmath>
 #include <cstddef>
+#include <cstdint>
 #include <ios>
 #include <iostream>
 #include <iterator>
+#include <limits>
 #include <memory>
 #include <queue>
 #include <sstream>
@@ -17,74 +21,43 @@
 template<typename T, typename... Args>
 T get_input(std::basic_istream<Args...>& is);
 
-namespace rectangle
-{
-
-using Rectangle = std::vector<std::pair<int, int>>;
-using point_type = std::pair<int, int>;
-using size_type = std::pair<std::size_t, std::size_t>;
-
-Rectangle make_rectangle();
-Rectangle make_rectangle(point_type left_up, point_type right_down);
-Rectangle make_rectangle(point_type left_up, size_type size);
-
-class Operation
+class BigInteger
 {
 public:
-  virtual ~Operation() noexcept;
-  virtual Rectangle operator()(const Rectangle&, const Rectangle&) const = 0;
-  virtual std::unique_ptr<Operation> copy() const = 0;
+  BigInteger();
+  BigInteger(std::uintmax_t init_value);
+
+  BigInteger& operator+=(const BigInteger& value);
+
+  template<typename... Args>
+  friend std::basic_ostream<Args...>& operator<<(std::basic_ostream<Args...>& os, const BigInteger& rhs);
+
+private:
+  std::valarray<std::uint_least16_t> values {};
 };
 
-Operation::~Operation() noexcept = default;
-
-class UnionOperation
-  : public Operation
+template<typename T>
+constexpr std::size_t bit_digit(T value, std::size_t bit_width = 2)
 {
-public:
-  Rectangle operator()(const Rectangle& lhs, const Rectangle& rhs) const override;
-  std::unique_ptr<Operation> copy() const override;
-};
-
-class ComplementOperation
-  : public Operation
-{
-public:
-  Rectangle operator()(const Rectangle& lhs, const Rectangle& rhs) const override;
-  std::unique_ptr<Operation> copy() const override;
-};
-
-std::istream& operator>>(std::istream& is, std::unique_ptr<Operation>& v);
-
-namespace square
-{
-
-Rectangle make_square(point_type left_up, std::size_t size);
+  std::size_t ans {0};
+  while (value >>= bit_width)
+    ++ans;
+  return ans + 1;
+}
 
 template<typename... Args>
-Rectangle make_square(std::basic_istream<Args...>& is);
-
-}
-}
+std::basic_ostream<Args...>& operator<<(std::basic_ostream<Args...>& os, const BigInteger& rhs);
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
 {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  rectangle::Rectangle calc_buf {};
-  std::queue<rectangle::Rectangle> rectangles;
-  std::queue<std::unique_ptr<rectangle::Operation>> operations;
-  for (auto i {get_input<std::size_t>(std::cin)}; i != 0; --i)
-  {
-    operations.push(get_input<std::unique_ptr<rectangle::Operation>>(std::cin));
-    rectangles.push(rectangle::square::make_square(std::cin));
-  }
-
-  for (; !operations.empty(); operations.pop(), rectangles.pop())
-    calc_buf = (*operations.front())(calc_buf, rectangles.front());
-
-  std::cout << calc_buf.size() << '\n';
+  constexpr auto init {0xffeedu};
+  std::cout << "init is : " << init << '\n';
+  std::cout << "init size is : " << bit_digit(init, 16) << std::endl;
+  BigInteger value {init};
+  std::cout << value << '\n';
 }
 
 template<typename T, typename... Args>
@@ -95,81 +68,31 @@ T get_input(std::basic_istream<Args...>& is)
   return value;
 }
 
-rectangle::Rectangle rectangle::make_rectangle()
-{
-  return {};
-}
+BigInteger::BigInteger() = default;
 
-rectangle::Rectangle rectangle::make_rectangle(point_type left_up, point_type right_down)
+BigInteger::BigInteger(std::uintmax_t init_value)
+  : values(bit_digit(init_value, 16))
 {
-  Rectangle v;
-  for (auto i {left_up.first}; i < right_down.first; ++i)
-    for (auto j {left_up.second}; j < right_down.second; ++j)
-      v.emplace_back(i, j);
-  return v;
-}
+  constexpr auto bitmask16 {0xffffu};
+  constexpr auto digits {16u};
 
-rectangle::Rectangle rectangle::make_rectangle(point_type left_up, size_type size)
-{
-  return make_rectangle(
-      left_up,
-      point_type{left_up.first + static_cast<point_type::first_type>(size.first),
-                 left_up.second + static_cast<point_type::second_type>(size.second)});
-}
-
-rectangle::Rectangle rectangle::UnionOperation::operator()(const Rectangle& lhs, const Rectangle& rhs) const
-{
-  Rectangle v;
-  std::set_union(lhs.cbegin(), lhs.cend(), rhs.cbegin(), rhs.cend(), std::back_inserter(v));
-  return v;
-}
-
-std::unique_ptr<rectangle::Operation> rectangle::UnionOperation::copy() const
-{
-  return std::make_unique<UnionOperation>();
-}
-
-rectangle::Rectangle rectangle::ComplementOperation::operator()(const Rectangle& lhs, const Rectangle& rhs) const
-{
-  Rectangle v;
-  std::set_difference(lhs.cbegin(), lhs.cend(), rhs.cbegin(), rhs.cend(), std::back_inserter(v));
-  return v;
-}
-
-std::unique_ptr<rectangle::Operation> rectangle::ComplementOperation::copy() const
-{
-  return std::make_unique<ComplementOperation>();
-}
-
-std::istream& rectangle::operator>>(std::istream& is, std::unique_ptr<Operation>& v)
-{
-  std::unordered_map<char, std::unique_ptr<Operation>> map;
-  map.emplace('+', std::make_unique<UnionOperation>());
-  map.emplace('-', std::make_unique<ComplementOperation>());
-
-  const auto ch {get_input<char>(is)};
-  const auto it {map.find(ch)};
-  if (it == map.end()) {
-    is.putback(ch);
-  } else {
-    v = it->second->copy();
+  for (auto& e : values) {
+    e = init_value & bitmask16;
+    init_value >>= digits;
   }
-  return is;
 }
 
-rectangle::Rectangle rectangle::square::make_square(point_type left_up, std::size_t size)
+BigInteger& BigInteger::operator+=(const BigInteger& rhs)
 {
-  return make_rectangle(
-      left_up,
-      point_type{left_up.first + static_cast<point_type::first_type>(size),
-                 left_up.second + static_cast<point_type::second_type>(size)});
+  return *this;
 }
 
 template<typename... Args>
-rectangle::Rectangle rectangle::square::make_square(std::basic_istream<Args...>& is)
+std::basic_ostream<Args...>& operator<<(std::basic_ostream<Args...>& os, const BigInteger& rhs)
 {
-  const auto left_point {get_input<int>(is)};
-  const auto up_point {get_input<int>(is)};
-  const auto size {get_input<std::size_t>(is)};
-  return make_square({left_point, up_point}, size);
+  if (rhs.values.size() == 0)
+    return os;
+  std::for_each(std::reverse_iterator{std::end(rhs.values)}, std::reverse_iterator{std::begin(rhs.values)},
+  [&os](const auto& e){os << e << '\t';});
+  return os;
 }
