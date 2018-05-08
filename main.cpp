5dbe7939ab970518caf9624a7d6c6671b4e646ca
Doi Yusuke <o111027y@mail.kyutech.jp>
2018-05-08 09:14:15 +0900

    Chage mod from max to 10000

--- main.cpp
+++ main.cpp
@@ -18,6 +18,12 @@
 #include <valarray>
 #include <vector>
 
+template<typename T>
+[[deprecated]]
+constexpr void f(T)
+{
+}
+
 template<typename T, typename... Args>
 T get_input(std::basic_istream<Args...>& is);
 
@@ -33,17 +39,15 @@ public:
   friend std::basic_ostream<Args...>& operator<<(std::basic_ostream<Args...>& os, const BigInteger& rhs);
 
 private:
+  void resize(std::size_t length);
+
+  static constexpr std::uint_least16_t mod {10000u};
+  static const auto digit {static_cast<std::uint_least16_t>(std::log10(mod))};
+
   std::valarray<std::uint_least16_t> values {};
 };
 
-template<typename T>
-constexpr std::size_t bit_digit(T value, std::size_t bit_width = 2)
-{
-  std::size_t ans {0};
-  while (value >>= bit_width)
-    ++ans;
-  return ans + 1;
-}
+const std::uint_least16_t BigInteger::digit;
 
 template<typename... Args>
 std::basic_ostream<Args...>& operator<<(std::basic_ostream<Args...>& os, const BigInteger& rhs);
@@ -53,11 +57,15 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
 
-  constexpr auto init {0xffeedu};
-  std::cout << "init is : " << init << '\n';
-  std::cout << "init size is : " << bit_digit(init, 16) << std::endl;
+  constexpr auto init {98765};
   BigInteger value {init};
-  std::cout << value << '\n';
+  std::cout << "first is          : " << value << '\n';
+  value += 50;
+  std::cout << "be added 50 is    : " << value << '\n';
+  value += 5000;
+  std::cout << "be added 5000 is  : " << value << '\n';
+  value += 10000;
+  std::cout << "be added 10000 is : " << value << '\n';
 }
 
 template<typename T, typename... Args>
@@ -71,19 +79,35 @@ T get_input(std::basic_istream<Args...>& is)
 BigInteger::BigInteger() = default;
 
 BigInteger::BigInteger(std::uintmax_t init_value)
-  : values(bit_digit(init_value, 16))
+  : values(static_cast<std::uintmax_t>(std::log10(static_cast<long double>(init_value))) / 4 + 1)
 {
-  constexpr auto bitmask16 {0xffffu};
-  constexpr auto digits {16u};
-
   for (auto& e : values) {
-    e = init_value & bitmask16;
-    init_value >>= digits;
+    e = static_cast<std::uint_least16_t>(init_value % mod);
+    init_value /= mod;
   }
 }
 
 BigInteger& BigInteger::operator+=(const BigInteger& rhs)
 {
+  if (values.size() < rhs.values.size())
+    resize(rhs.values.size());
+
+  bool carry {false};
+  auto it {std::begin(values)};
+  std::for_each(std::begin(rhs.values), std::end(rhs.values), [&carry, &it](const auto& e){
+    carry = mod <= (*it += e + (carry ? 1 : 0));
+    if (carry)
+      *it -= mod;
+    ++it;
+  });
+  if (carry) {
+    if (it == std::end(values)) {
+      resize(values.size() + 1);
+      it = std::end(values);
+      --it;
+    }
+    *it += 1;
+  }
   return *this;
 }
 
@@ -92,7 +116,15 @@ std::basic_ostream<Args...>& operator<<(std::basic_ostream<Args...>& os, const B
 {
   if (rhs.values.size() == 0)
     return os;
+  bool isfirst {false};
   std::for_each(std::reverse_iterator{std::end(rhs.values)}, std::reverse_iterator{std::begin(rhs.values)},
-  [&os](const auto& e){os << e << '\t';});
+  [&os, &isfirst](const auto& e){os << (std::exchange(isfirst, true) ? ' ' : '\0') << e;});
   return os;
+}
+
+void BigInteger::resize(std::size_t length)
+{
+  const auto tmp {values};
+  values.resize(length);
+  std::copy(std::begin(tmp), std::end(tmp), std::begin(values));
 }
