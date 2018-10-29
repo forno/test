f59593e2998d9b69fbd38b82d395e795be033731
Doi Yusuke <doi.yusuke806@mail.kyutech.jp>
2018-10-30 07:39:07 +0900

    Try rdf raptor libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <experimental/algorithm>
 #include <cassert>
 #include <cstddef>
 #include <cstdint>
@@ -12,50 +11,178 @@
 #include <string>
 #include <valarray>
 
+#include <redland.h>
+
+using namespace std;
+
 int main(int argc, char** argv)
 {
-  if (argc < 3) {
-    std::cerr << "I need arguments\n"
-                 "[Usage]   execute [data size] [rest percent]\n"
-                 "[Example] execute 100000 0.7\n";
-    return EXIT_FAILURE;
+  librdf_world* world;
+  librdf_storage* storage;
+  librdf_parser* parser;
+  librdf_model* model;
+  librdf_stream* stream;
+  librdf_node *subject, *predicate;
+  librdf_iterator* iterator;
+  librdf_statement *partial_statement, *statement2;
+  char *program=argv[0];
+  librdf_uri *uri;
+  char *parser_name=NULL;
+  int count;
+  raptor_world *raptor_world_ptr;
+  raptor_iostream* iostr;
+
+  if(argc <2 || argc >3) {
+    fprintf(stderr, "USAGE: %s: <RDF source URI> [rdf parser name]\n", program);
+    return(1);
   }
 
-  std::size_t length {std::stoul(argv[1])};
-  assert(length < std::numeric_limits<std::uint_fast32_t>::max());
 
-  auto percent {std::stof(argv[2])};
-  assert(percent <= 1);
+  world = librdf_new_world();
+  librdf_world_open(world);
+  raptor_world_ptr = librdf_world_get_raptor(world);
 
-  std::valarray<std::uint_fast32_t> a(length);
-  std::iota(std::begin(a), std::end(a), 1);
+  uri=librdf_new_uri(world, (const unsigned char*)argv[1]);
+  if(!uri) {
+    fprintf(stderr, "%s: Failed to create URI\n", program);
+    return(1);
+  }
 
-  std::valarray<std::uint_fast32_t> ans(
-    static_cast<std::size_t>(static_cast<decltype(percent)>(a.size()) * percent));
-  std::experimental::sample(
-    std::begin(a),
-    std::end(a),
-    std::begin(ans),
-    ans.size(),
-    std::default_random_engine{std::random_device{}()});
+  storage=librdf_new_storage(world, "memory", "test", NULL);
+  if(!storage) {
+    fprintf(stderr, "%s: Failed to create new storage\n", program);
+    return(1);
+  }
 
-  std::sort(std::begin(ans), std::end(ans));
-  std::valarray<std::uint_fast32_t> dis(ans.size());
-  std::adjacent_difference(std::begin(ans), std::end(ans), std::begin(dis));
-  dis -= 1;
+  model=librdf_new_model(world, storage, NULL);
+  if(!model) {
+    fprintf(stderr, "%s: Failed to create model\n", program);
+    return(1);
+  }
 
-  std::map<uint_fast32_t, uint_fast32_t> res;
-  for (auto e : dis)
-    ++res[e];
 
-  for (auto e : res)
-    std::cout << e.first << ',' << e.second << '\n';
+  if(argc==3)
+    parser_name=argv[2];
 
-  auto ave {0.0};
-  const auto size {static_cast<decltype(ave)>(dis.size())};
-  for (auto e : res)
-    ave += static_cast<decltype(ave)>(e.first) *
-           static_cast<decltype(ave)>(e.second) / size;
+  parser=librdf_new_parser(world, parser_name, NULL, NULL);
+  if(!parser) {
+    fprintf(stderr, "%s: Failed to create new parser\n", program);
+    return(1);
+  }
 
-  std::cout << "Average: " << ave << '\n';
+
+  /* PARSE the URI as RDF/XML*/
+  fprintf(stdout, "%s: Parsing URI %s\n", program, librdf_uri_as_string(uri));
+  if(librdf_parser_parse_into_model(parser, uri, uri, model)) {
+    fprintf(stderr, "%s: Failed to parse RDF into model\n", program);
+    return(1);
+  }
+  librdf_free_parser(parser);
+
+
+  statement2=librdf_new_statement_from_nodes(world, librdf_new_node_from_uri_string(world, (const unsigned char*)"http://www.dajobe.org/"),
+      librdf_new_node_from_uri_string(world, (const unsigned char*)"http://purl.org/dc/elements/1.1/title"),
+      librdf_new_node_from_literal(world, (const unsigned char*)"My Home Page", NULL, 0)
+      );
+  librdf_model_add_statement(model, statement2);
+
+  /* Free what we just used to add to the model - now it should be stored */
+  librdf_free_statement(statement2);
+
+
+  /* Print out the model*/
+  fprintf(stdout, "%s: Resulting model is:\n", program);
+  iostr = raptor_new_iostream_to_file_handle(raptor_world_ptr, stdout);
+  librdf_model_write(model, iostr);
+  raptor_free_iostream(iostr);
+
+
+  /* Construct the query predicate (arc) and object (target) 
+   * and partial statement bits
+   */
+  subject=librdf_new_node_from_uri_string(world, (const unsigned char*)"http://www.dajobe.org/");
+  predicate=librdf_new_node_from_uri_string(world, (const unsigned char*)"http://purl.org/dc/elements/1.1/title");
+  if(!subject || !predicate) {
+    fprintf(stderr, "%s: Failed to create nodes for searching\n", program);
+    return(1);
+  }
+  partial_statement=librdf_new_statement(world);
+  librdf_statement_set_subject(partial_statement, subject);
+  librdf_statement_set_predicate(partial_statement, predicate);
+
+
+  /* QUERY TEST 1 - use find_statements to match */
+
+  fprintf(stdout, "%s: Trying to find_statements\n", program);
+  stream=librdf_model_find_statements(model, partial_statement);
+  if(!stream) {
+    fprintf(stderr, "%s: librdf_model_find_statements returned NULL stream\n", program);
+  } else {
+    count=0;
+    while(!librdf_stream_end(stream)) {
+      librdf_statement *statement=librdf_stream_get_object(stream);
+      if(!statement) {
+        fprintf(stderr, "%s: librdf_stream_next returned NULL\n", program);
+        break;
+      }
+
+      fputs("  Matched statement: ", stdout);
+      librdf_statement_print(statement, stdout);
+      fputc('\n', stdout);
+
+      librdf_stream_next(stream);
+      count++;
+    }
+    librdf_free_stream(stream);  
+    fprintf(stderr, "%s: got %d matching statements\n", program, count);
+  }
+
+
+  /* QUERY TEST 2 - use get_targets to do match */
+  fprintf(stdout, "%s: Trying to get targets\n", program);
+  iterator=librdf_model_get_targets(model, subject, predicate);
+  if(!iterator)  {
+    fprintf(stderr, "%s: librdf_model_get_targets failed to return iterator for searching\n", program);
+    return(1);
+  }
+
+  count=0;
+  while(!librdf_iterator_end(iterator)) {
+    librdf_node *target;
+
+    target=(librdf_node*)librdf_iterator_get_object(iterator);
+    if(!target) {
+      fprintf(stderr, "%s: librdf_iterator_get_object returned NULL\n", program);
+      break;
+    }
+
+    fputs("  Matched target: ", stdout);
+    librdf_node_print(target, stdout);
+    fputc('\n', stdout);
+
+    count++;
+    librdf_iterator_next(iterator);
+  }
+  librdf_free_iterator(iterator);
+  fprintf(stderr, "%s: got %d target nodes\n", program, count);
+
+  librdf_free_statement(partial_statement);
+  /* the above does this since they are still attached */
+  /* librdf_free_node(predicate); */
+  /* librdf_free_node(object); */
+
+  librdf_free_model(model);
+
+  librdf_free_storage(storage);
+
+  librdf_free_uri(uri);
+
+  librdf_free_world(world);
+
+#ifdef LIBRDF_MEMORY_DEBUG
+  librdf_memory_report(stderr);
+#endif
+
+  /* keep gcc -Wall happy */
+  return(0);
 }
