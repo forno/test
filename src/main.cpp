1551ab6b346818c094c3996a51eeb5a04f2df531
Doi Yusuke <doi.yusuke806@mail.kyutech.jp>
2018-10-30 06:11:33 +0900

    Fix format

--- src/main.cpp
+++ src/main.cpp
@@ -30,12 +30,19 @@ int main(int argc, char** argv)
   std::valarray<std::uint_fast32_t> a(length);
   std::iota(std::begin(a), std::end(a), 1);
 
-  std::valarray<std::uint_fast32_t> ans(static_cast<std::size_t>(static_cast<decltype(percent)>(a.size()) * percent));
-  std::experimental::sample(std::begin(a), std::end(a), std::begin(ans), ans.size(), std::default_random_engine{std::random_device{}()});
+  std::valarray<std::uint_fast32_t> ans(
+    static_cast<std::size_t>(static_cast<decltype(percent)>(a.size()) * percent));
+  std::experimental::sample(
+    std::begin(a),
+    std::end(a),
+    std::begin(ans),
+    ans.size(),
+    std::default_random_engine{std::random_device{}()});
 
   std::sort(std::begin(ans), std::end(ans));
   std::valarray<std::uint_fast32_t> dis(ans.size());
   std::adjacent_difference(std::begin(ans), std::end(ans), std::begin(dis));
+  dis -= 1;
 
   std::map<uint_fast32_t, uint_fast32_t> res;
   for (auto e : dis)
@@ -45,9 +52,10 @@ int main(int argc, char** argv)
     std::cout << e.first << ',' << e.second << '\n';
 
   auto ave {0.0};
-  std::for_each(std::next(std::begin(res)), std::end(res), [&ave, size = static_cast<decltype(ave)>(dis.size())](auto e) {
-    ave += static_cast<decltype(ave)>(e.first - 1) * static_cast<decltype(ave)>(e.second) / size;
-  });
+  const auto size {static_cast<decltype(ave)>(dis.size())};
+  for (auto e : res)
+    ave += static_cast<decltype(ave)>(e.first) *
+           static_cast<decltype(ave)>(e.second) / size;
 
   std::cout << "Average: " << ave << '\n';
 }
