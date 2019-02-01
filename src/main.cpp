b5674298cb6097562648590260b3547125f21c08
Doi Yusuke <doi.yusuke806@mail.kyutech.jp>
2019-02-01 13:27:10 +0900

    Paiza B: エンジニアが死滅した世界.砂漠の公園 failed

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,76 @@
-#include <algorithm>
-#include <cassert>
-#include <cstddef>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
-#include <limits>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <random>
-#include <string>
-#include <string_view>
-#include <utility>
-#include <valarray>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-#include <redland.h>
-
-int main(int argc, char** argv)
+class Score
 {
-  const std::shared_ptr<int> p {new int};
-  *p = 5;
-  std::cout << *p << '\n';
+public:
+    int total() const
+    {
+        return total_;
+    }
+    
+    int win() const
+    {
+        return win_;
+    }
+    
+    int draw() const
+    {
+        return draw_;
+    }
+    
+    int lose() const
+    {
+        return total_ - win_ * 2 - draw_;
+    }
+    
+    void add(const char c)
+    {
+        switch (c) {
+            case 'W':
+            ++win_;
+            total_ += 2;
+            break;
+            case 'D':
+            ++draw_;
+            ++total_;
+            break;
+            default:
+            ;
+        }
+    }
+private:
+    int total_;
+    int win_;
+    int draw_;
+};
+
+namespace std
+{
+    inline std::ostream& operator<<(std::ostream& os, const Score& s)
+    {
+        os << s.total() << ' ' << s.win() << ' ' << s.draw() << ' ' << s.lose();
+        return os;
+    }
+}
+
+int main(void){
+    std::size_t length;
+    std::cin >> length;
+    std::vector<Score> scores(length);
+    for (auto& v : scores) {
+        for (auto i {length}; i; --i) {
+            char c;
+            std::cin >> c;
+            v.add(c);
+        }
+    }
+
+    const auto max_score {max_element(scores.begin(), scores.end(), [](const auto& lhs, const auto& rhs){
+        return lhs.total() < rhs.total();
+    })};
+    cout << max_score - scores.begin() + 1 << ' ' << *max_score;
+    return 0;
 }
