0fb9ebd2637e57357827372a9a8accfbfd0b4e1a
Doi Yusuke <doi.yusuke806@mail.kyutech.jp>
2019-02-26 21:15:36 +0900

    Beast working

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,73 @@
-#include <algorithm>
-#include <cassert>
-#include <cmath>
-#include <cstddef>
-#include <cstdint>
-#include <cstdio>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
 #include <cstdlib>
 #include <iostream>
-#include <limits>
-#include <map>
-#include <memory>
-#include <numeric>
-#include <random>
 #include <string>
-#include <string_view>
-#include <utility>
-#include <valarray>
 
-template<typename T>
-constexpr T get_value(std::istream& is)
-{
-  T v {};
-  is >> v;
-  return v;
-}
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+namespace http = boost::beast::http;    // from <boost/beast/http.hpp>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+// Performs an HTTP GET and prints the response
+int main(int argc, char** argv)
 {
-	int A, B, C, X;
-	int total;
-	int ans = 0;
-	std::cin >> A >> B >> C >> X;
-	for (int i = 0; i <= A; ++i) {
-    std::cout << "i";
-		for (int j = 0; j <= B; ++j) {
-      std::cout << "j";
-			for (int k = 0; k <= C; ++k) {
-        std::cout << "k";
-				total = 500*i + 100*j + 50*k;
-				if (total == X) ++ans;
-			}
-		}
-    std::cout << '\n';
+	try {
+		auto const host = "example.com";
+		auto const port = "80";
+		auto const target = "/";
+		int version {11};
+
+		// The io_context is required for all I/O
+		boost::asio::io_context ioc;
+
+		// These objects perform our I/O
+		tcp::resolver resolver{ioc};
+		tcp::socket socket{ioc};
+
+		// Look up the domain name
+		auto const results = resolver.resolve(host, port);
+
+		// Make the connection on the IP address we get from a lookup
+		boost::asio::connect(socket, results.begin(), results.end());
+
+		// Set up an HTTP GET request message
+		http::request<http::string_body> req{http::verb::get, target, version};
+		req.set(http::field::host, host);
+		req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+		// Send the HTTP request to the remote host
+		http::write(socket, req);
+
+		// This buffer is used for reading and must be persisted
+		boost::beast::flat_buffer buffer;
+
+		// Declare a container to hold the response
+		http::response<http::dynamic_body> res;
+
+		// Receive the HTTP response
+		http::read(socket, buffer, res);
+
+		// Write the message to standard out
+		std::cout << res << std::endl;
+
+		// Gracefully close the socket
+		boost::system::error_code ec;
+		socket.shutdown(tcp::socket::shutdown_both, ec);
+
+		// not_connected happens sometimes
+		// so don't bother reporting it.
+		//
+		if(ec && ec != boost::system::errc::not_connected)
+			throw boost::system::system_error{ec};
+
+		// If we get here then the connection is closed gracefully
 	}
-	std::cout << ans << std::endl;
-	return 0;
+	catch(std::exception const& e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
