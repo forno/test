a609b7f8df3a3bf360a462aa53fdc89295903cf8
FORNO <forno@xmaho.link>
2020-01-17 02:56:00 +0900

    Test for implimantation

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,16 @@ int main() {
   cin.sync_with_stdio(false);
   cin.tie(nullptr);
 
-  vector<int> cities(get_value<size_t>(cin));
-  for (auto i {get_value<size_t>(cin)}; i != 0; --i) {
-    const auto a {get_value<int>(cin)}, b {get_value<int>(cin)};
-    ++cities[a-1];
-    ++cities[b-1];
+  int A, B, C, X;
+  cin >> A >> B >> C >> X;
+
+  auto res {0u};
+  for (auto a {0}; a < A; ++a) {
+    const auto left {X - 500 * a};
+    res += clamp(left / 100 + 1, 0, B + 1) - clamp((left - (C - 1) * 50) / 100, 0, B + 1);
   }
 
-  for (auto e : cities)
-    cout << e << '\n';
+  cout << res << '\n';
   
   return 0;
 }
