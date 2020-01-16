0c4bd85bd542cd0f3d0bffb0b8087593c682a09b
FORNO <forno@xmaho.link>
2020-01-17 04:17:05 +0900

    Make template

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,15 @@ using namespace std::literals::string_literals;
 template<typename T>
 T get_value(std::istream& is);
 
-int main() {
+void task()
+{
+}
+
+int main()
+{
   cin.sync_with_stdio(false);
   cin.tie(nullptr);
-
-  int A, B, C, X;
-  cin >> A >> B >> C >> X;
-
-  auto res {0u};
-  for (auto a {0}; a < A; ++a) {
-    const auto left {X - 500 * a};
-    res += clamp(left / 100 + 1, 0, B + 1) - clamp((left - (C - 1) * 50) / 100, 0, B + 1);
-  }
-
-  cout << res << '\n';
-  
+  task();
   return 0;
 }
 
