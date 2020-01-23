ce47a56f8f198c7fb99ed68ea4d6b37febfc03c2
FORNO <forno@xmaho.link>
2020-01-23 22:09:09 +0900

    Atcoder 151 d

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@ using ll = long long;
 
 namespace forno
 {
-template<typename T, typename... Args>
-T get_value(std::basic_istream<Args...>& is = std::cin);
-template<typename C, typename... Args>
-C get_container(std::basic_istream<Args...>& is = std::cin, typename C::size_type length = std::numeric_limits<typename C::size_type>::max());
+template<typename T>
+T get_value(std::istream& is = std::cin);
+template<typename C>
+C get_container(std::istream& is = std::cin, typename C::size_type length = std::numeric_limits<typename C::size_type>::max());
 template<typename C>
 ll llsize(const C& c);
 template<typename C, size_t N>
@@ -25,41 +25,28 @@ ll lllog2(ll x);
 }
 using namespace forno;
 
-template<typename T>
-class myvec : public std::vector<T>
-{
-public:
-  void push_back(const T& x)
-  {
-    cout << "myvec push_back\n";
-    vector<T>::push_back(x);
-  }
-
-  void push_back(T&& x)
-  {
-    cout << "myvec push_back with move\n";
-    vector<T>::push_back(std::move(x));
-  }
-
-  vector<T>::iterator insert(vector<T>::const_iterator it, const T& x)
-  {
-    cout << "myvec insert\n";
-    return vector<T>::insert(it, x);
-  }
-
-  vector<T>::iterator insert(vector<T>::const_iterator it, T&& x)
-  {
-    cout << "myvec insert with move\n";
-    return vector<T>::insert(it, std::move(x));
-  }
-};
-
 int main()
 {
-  istringstream iss {"1 1 1 1 1 1 1 1 1 1 1 1 1 1 1"s};
-  get_container<vector<int>>(cin, 5);
-  get_container<myvec<int>>(cin, 5);
-  get_container<unordered_set<int>>(cin, 5);
+  const auto n {get_value<ll>()};
+  const auto m {get_value<ll>()};
+  std::vector<bool> ans(n);
+  auto w {0u};
+  for (auto i {m}; i != 0; --i) {
+    const auto index {get_value<int>() - 1};
+    const auto score {get_value<string>()};
+    if (ans[index])
+      break;
+    if (score == "WA") {
+      ++w;
+    } else {
+      ans[index] = true;
+    }
+  }
+  auto sum {0u};
+  for (auto e : ans)
+    sum += e;
+  cout << sum << ' ' << w << '\n';
+
   return 0;
 }
 
@@ -79,55 +66,31 @@ public:
   {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    std::cout << std::boolalpha;
     std::cout.imbue(std::locale(std::cout.getloc(), new custom_numpunct{}));
+    std::cout << std::boolalpha << std::fixed << std::setprecision(15);
   }
 } initalize;
 
 namespace forno
 {
 
-template<typename T, typename... Args>
-T get_value(std::basic_istream<Args...>& is)
+template<typename T>
+T get_value(std::istream& is)
 {
   T v {};
   is >> v;
   return v;
 }
 
-namespace detail
-{
-
-template<typename T>
-auto has_push_back_impl(nullptr_t) -> decltype(
-  std::declval<T>().push_back(std::declval<typename T::value_type>()),
-  std::true_type{});
-
-template<typename T>
-auto has_push_back_impl(...) -> std::false_type;
-
-template<typename T>
-struct has_push_back
-  : decltype(has_push_back_impl<T>(nullptr))
-{};
-
-}
-
-template<typename C, typename... Args>
-C get_container(std::basic_istream<Args...>& is, typename C::size_type length)
+template<typename C>
+C get_container(std::istream& is, typename C::size_type length)
 {
   C v {};
   if (length != std::numeric_limits<typename C::size_type>::max())
     v.reserve(length);
   typename C::value_type e {};
-  for (auto i {length}; i != 0 && is >> e; --i) {
-    if constexpr (detail::has_push_back<C>{}) {
-      v.push_back(std::move_if_noexcept(e));
-    } else {
-      using std::cend;
-      v.insert(cend(v), std::move_if_noexcept(e));
-    }
-  }
+  for (auto i {length}; i != 0 && is >> e; --i)
+    v.push_back(std::move_if_noexcept(e));
   return v;
 }
 
