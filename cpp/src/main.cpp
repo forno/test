3d3cb772549f20cb77f6333be922853392399f3f
FORNO <forno@xmaho.link>
2021-06-07 01:50:25 +0900

    Ochinchin percent
    Signed-off-by: FORNO <forno@xmaho.link>

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,32 +1,57 @@
 #include <bits/stdc++.h>
-//#include <boost/range/irange.hpp>
 using namespace std::literals;
 using namespace std;
 
 namespace xmaho
 {
-inline namespace input
-{
-template<typename T>
-T get_value(std::istream& is = std::cin);
-template<typename C>
-C get_container(std::istream& is = std::cin, typename C::size_type length = std::numeric_limits<typename C::size_type>::max());
-}
+  inline namespace input
+  {
+    template <typename T>
+    T get_value(std::istream &is = std::cin);
+    template <typename C>
+    C get_container(std::istream &is = std::cin, typename C::size_type length = std::numeric_limits<typename C::size_type>::max());
+  }
 }
 using namespace xmaho;
 
 int main()
 {
-  std::cout << "Hello world!" << std::endl;
+  default_random_engine engine{random_device{}()};
+  uniform_int_distribution<> dist{0, 5};
+  const auto get_dice = [&dist, &engine](auto)
+  {
+    return dist(engine);
+  };
+
+  const auto test_ochinchin = [&get_dice](auto)
+  {
+    unordered_map<int, uint> dice_count{};
+
+    for (const auto dice : views::iota(0, 5) | views::transform(get_dice))
+    {
+      ++dice_count[dice];
+    }
+
+    return dice_count[0] == 1 && dice_count[1] == 2 && dice_count[2] == 2;
+  };
+
+  const auto try_count = get_value<uint>(cin);
+  const auto ochinchin_count = ranges::count(
+      views::iota(0u, try_count) | views::transform(test_ochinchin),
+      true);
+
+  cout << ochinchin_count << '/' << try_count << '\n'
+       << (double)ochinchin_count / try_count * 100. << endl;
   return 0;
 }
 
 class initalization
 {
-  class custom_numpunct: public std::numpunct<char>
+  class custom_numpunct : public std::numpunct<char>
   {
   public:
     using numpunct::numpunct;
+
   protected:
     std::string do_truename() const override { return "Yes"; }
     std::string do_falsename() const override { return "No"; }
@@ -42,18 +67,22 @@ public:
   }
 } initalize;
 
-template<typename T>
-T xmaho::input::get_value(std::istream& is)
-{ T v {}; is >> v; return v; }
-
-template<typename C>
-C xmaho::input::get_container(std::istream& is, typename C::size_type length)
+template <typename T>
+T xmaho::input::get_value(std::istream &is)
 {
-  C v {};
+  T v{};
+  is >> v;
+  return v;
+}
+
+template <typename C>
+C xmaho::input::get_container(std::istream &is, typename C::size_type length)
+{
+  C v{};
   if (length != std::numeric_limits<typename C::size_type>::max())
     v.reserve(length);
-  typename C::value_type e {};
-  for (auto i {length}; i != 0 && is >> e; --i)
+  typename C::value_type e{};
+  for (auto i{length}; i != 0 && is >> e; --i)
     v.push_back(std::move_if_noexcept(e));
   return v;
 }
